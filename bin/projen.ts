b38a6dce56b9dec8a142bd012afbafc7c8aff1f8
Elad Ben-Israel <benisrae@amazon.com>
2020-05-12 10:21:04 +0300

    feat: support bootstrapping from an empty repo
    basically always symlink projen to node_modules so we can run .projenrc.js

---
+++ bin/projen.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { PROJEN_RC } from '../lib/common';
+
+const projenfile = path.resolve(PROJEN_RC);
+
+if (!fs.existsSync(projenfile)) {
+  console.error(`unable to find ${projenfile}`);
+  process.exit(1);
+}
+
+// now in order to run, projen needs to be able to "require('projen')". this will
+// work if we already installed our modules, but will fail miserably when we
+// bootstrap a checked out (or empty) repo.
+const projen = path.dirname(require.resolve('../package.json'));
+const symlink = path.join('node_modules', 'projen');
+if (!fs.existsSync(symlink)) {
+  fs.mkdirSync('node_modules', { recursive: true });
+  fs.symlinkSync(projen, symlink);
+}
+
+// now we are ready to rock
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require(projenfile);
