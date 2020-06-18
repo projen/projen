204358275b2a2c32967c8293f96671c6ac09201a
Elad Ben-Israel <benisrae@amazon.com>
2020-06-18 14:21:48 +0300

    feat(eslint): consistent quote-props

--- lib/eslint.ts
+++ lib/eslint.ts
@@ -59,7 +59,9 @@ export class Eslint extends Construct {
           // Style
           'quotes': [ 'error', 'single', { avoidEscape: true } ],
           'comma-dangle': [ 'error', 'always-multiline' ], // ensures clean diffs, see https://medium.com/@nikgraf/why-you-should-enforce-dangling-commas-for-multiline-statements-d034c98e36f8
-      
+          'quote-props': [ 'error', 'consistent-as-needed', { 'unnecessary': true } ],
+          'indent': [ 'error', 2 ],
+
           // Require all imported dependencies are actually declared in package.json
           'import/no-extraneous-dependencies': [
             'error',
