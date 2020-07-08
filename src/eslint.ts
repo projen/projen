01b107769dedd83be8666d3a2c73fa0a1dc74090
andrestone <andrestone@users.noreply.github.com>
2020-07-08 14:36:26 +0200

    glob pattern

--- src/eslint.ts
+++ src/eslint.ts
@@ -90,8 +90,8 @@ export class Eslint extends Construct {
         ignorePatterns: [
           '*.js',
           '*.d.ts',
-          'node_modules/',
-          'templates/',
+          'node_modules/', // docs say ignored it's by default?
+          'templates/**',
           '*.generated.ts',
           'coverage',
         ],
