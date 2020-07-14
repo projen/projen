a7d3b93f2a99984e8ac049af6cf7e4565439ac4b
Elad Ben-Israel <benisrae@amazon.com>
2020-07-14 14:30:05 +0300

    feat: allow "init" in a non-empty directory

--- src/cmds/init.ts
+++ src/cmds/init.ts
@@ -2,9 +2,9 @@ import * as fs from 'fs-extra';
 import * as yargs from 'yargs';
 import {availableTemplates, templatesPath} from '../cli';
 import * as path from 'path';
+import { PROJEN_RC } from '../common';
 
 class Command implements yargs.CommandModule {
-
   public readonly command = 'init TEMPLATE';
   public readonly describe = 'Initializes a projen project using a template.';
   public readonly builder = (args: yargs.Argv) => args
@@ -13,21 +13,17 @@ class Command implements yargs.CommandModule {
     .choices('TEMPLATE', availableTemplates);
 
   public async handler(args: any) {
-    // fails if .projenrc.js already exists
-    if (fs.readdirSync('.').filter(f => f !== 'node_modules').length > 0) {
-      process.stderr.write('Cannot initialize a projen project in a non-empty directory.\n');
+    // fail if .projenrc.js already exists
+    if (fs.existsSync(PROJEN_RC)) {
+      console.error(`Directory already contains ${PROJEN_RC}`);
       process.exit(1);
     }
+
     const templatePath = path.join(templatesPath, args['TEMPLATE']);
-    try {
-      fs.copySync(templatePath, '.', {recursive: true});
-      process.stdout.write(`Projen project generated from the "${args['TEMPLATE']}" template. \n` +
-        'Now, go edit it (nothing there is what you want).\n' + 
-        'When you are done, simply run "npx projen" to get the magic going.\n');
-    } catch (e) {
-      process.stderr.write(`Error when trying to copy template files: ${e}\n`);
-      throw e;
-    }
+    fs.copySync(templatePath, '.', {recursive: true});
+    console.error(`${PROJEN_RC} generated from the "${args['TEMPLATE']}" template.`);
+    console.error('Now, go edit it (nothing there is what you want).');
+    console.error('When you are done, simply run "npx projen" to get the magic going.');
   }
 }
 
