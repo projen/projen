5d7e1f7b797f271a3f550d4f1d63c324db7bfa24
Elad Ben-Israel <benisrae@amazon.com>
2020-07-15 13:32:03 +0300

    add test for pjid list

--- test/inventory.test.ts
+++ test/inventory.test.ts
@@ -1,7 +1,17 @@
 import * as inventory from '../src/inventory';
 
+const result = inventory.discover();
+
+test('project id', () => {
+  expect(result.map(x => x.pjid)).toStrictEqual([
+    'jsii',
+    'node',
+    'project',
+    'ts',
+  ]);
+})
+
 test('inventory', () => {
-  const result = inventory.discover();
   expect(result).toMatchSnapshot();
 });
 
