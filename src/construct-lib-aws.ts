cbc3eba34084983256099a3a0e3262f0780d8bdc
Elad Ben-Israel <benisrae@amazon.com>
2020-08-27 03:04:54 +0300

    chore: add example to construct-lib-aws

--- src/construct-lib-aws.ts
+++ src/construct-lib-aws.ts
@@ -35,6 +35,64 @@ export interface ConstructLibraryAwsOptions extends ConstructLibraryOptions {
  * use within the AWS CDK with a friendly workflow and automatic publishing to
  * the construct catalog.
  *
+ * ```ts
+ * const project = new ConstructLibraryAws({
+ *   name: 'cdk-watchful',
+ *   description: 'Watching your CDK apps since 2019',
+ *   jsiiVersion: Semver.caret('1.7.0'),
+ *   authorName: 'Elad Ben-Israel',
+ *   authorEmail: 'elad.benisrael@gmail.com',
+ *   repository: 'https://github.com/eladb/cdk-watchful.git',
+ *   keywords: [
+ *     "cloudwatch",
+ *     "monitoring"
+ *   ],
+ *
+ *   catalog: {
+ *     twitter: 'emeshbi'
+ *   },
+ *
+ *   // creates PRs for projen upgrades
+ *   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+ *
+ *   cdkVersion: '1.54.0',
+ *   cdkDependencies: [
+ *     "@aws-cdk/aws-apigateway",
+ *     "@aws-cdk/aws-cloudwatch",
+ *     "@aws-cdk/aws-cloudwatch-actions",
+ *     "@aws-cdk/aws-dynamodb",
+ *     "@aws-cdk/aws-ecs",
+ *     "@aws-cdk/aws-ecs-patterns",
+ *     "@aws-cdk/aws-elasticloadbalancingv2",
+ *     "@aws-cdk/aws-events",
+ *     "@aws-cdk/aws-events-targets",
+ *     "@aws-cdk/aws-lambda",
+ *     "@aws-cdk/aws-rds",
+ *     "@aws-cdk/aws-sns",
+ *     "@aws-cdk/aws-sns-subscriptions",
+ *     "@aws-cdk/aws-sqs",
+ *     "@aws-cdk/core"
+ *   ],
+ *   devDependencies: {
+ *     "aws-sdk": Semver.caret("2.708.0")
+ *   },
+ *
+ *   // jsii publishing
+ *
+ *   java: {
+ *     javaPackage: 'com.github.eladb.watchful',
+ *     mavenGroupId: 'com.github.eladb',
+ *     mavenArtifactId: 'cdk-watchful'
+ *   },
+ *   python: {
+ *     distName: 'cdk-watchful',
+ *     module: 'cdk_watchful'
+ *   }
+ * });
+ *
+ * project.synth();
+ * ```
+ *
  * @pjid construct-lib-aws
  */
 export class ConstructLibraryAws extends ConstructLibrary {
