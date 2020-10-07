projen/projen

commit f2a03d3b1f554df41142112d03f6c55786258a75

feat(node): support 'npm' as a package manager (#157)
Adds support for specifying `packageManager` for node projects (and derivatives). The default is still `yarn` but `npm` is also supported.

Closes #156

src/node-project.ts
@@ -17,6 +17,21 @@ import { Version } from './version';
 
 const PROJEN_SCRIPT = 'projen';
 
+/**
+ * The node package manager to use.
+ */
+export enum NodePackageManager {
+  /**
+   * Use `yarn` as the package manager.
+   */
+  YARN,
+
+  /**
+   * Use `npm` as the package manager.
+   */
+  NPM
+}
+
 export interface NodeProjectCommonOptions {
   readonly bundledDependencies?: string[];
   readonly dependencies?: Record<string, Semver>;
@@ -155,6 +170,13 @@ export interface NodeProjectCommonOptions {
    * @default "registry.npmjs.org"
    */
   readonly npmRegistry?: string;
+
+  /**
+   * The Node Package Manager used to execute scripts
+   *
+   * @default packageManager.YARN
+   */
+  readonly packageManager?: NodePackageManager;
 
   /**
    * License copyright owner.
@@ -447,6 +469,16 @@ export class NodeProject extends Project {
 
   protected readonly npmRegistry: string;
 
+  /**
+   * The package manager to use.
+   */
+  protected readonly packageManager: NodePackageManager;
+
+  /**
+   * The command to use to run scripts (e.g. `yarn run` or `npm run` depends on the package mabnager).
+   */
+  public readonly runScriptCommand: string;
+
   constructor(options: NodeProjectOptions) {
     super();
 
@@ -473,6 +505,16 @@ export class NodeProject extends Project {
     this.npmDistTag = options.npmDistTag ?? 'latest';
 
     this.npmRegistry = options.npmRegistry ?? 'registry.npmjs.org';
+
+    this.packageManager = options.packageManager ?? NodePackageManager.YARN;
+
+    this.runScriptCommand = (() => {
+      switch (this.packageManager) {
+        case NodePackageManager.NPM: return 'npm run';
+        case NodePackageManager.YARN: return 'yarn run';
+        default: throw new Error(`unexpected package manager ${this.packageManager}`);
+      }
+    })();
 
     this.scripts = {};
 
@@ -1004,8 +1046,6 @@ export class NodeProject extends Project {
   public postSynthesize(outdir: string) {
     super.postSynthesize(outdir);
 
-    const install = ['yarn install'];
-
     // now we run `yarn install`, but before we do that, remove the
     // `node_modules/projen` symlink so that yarn won't hate us.
     const projenModule = path.resolve('node_modules', 'projen');
@@ -1015,18 +1055,28 @@ export class NodeProject extends Project {
       }
     } catch (e) { }
 
-    // add --check-files to ensure all modules exist (especiall projen which was just removed).
-    install.push('--check-files');
-
-    // if we are running in a CI environment, fix versions through the lockfile.
-    if (process.env.CI) {
-      logging.info('Running yarn with --frozen-lockfile since "CI" is defined.');
-      install.push('--frozen-lockfile');
-    }
-
-    exec(install.join(' '), { cwd: outdir });
+    exec(this.installDepsCommand, { cwd: outdir });
 
     this.resolveDependencies(outdir);
+  }
+
+  private get installDepsCommand() {
+    switch (this.packageManager) {
+      case NodePackageManager.YARN:
+        return [
+          'yarn install',
+          '--check-files', // ensure all modules exist (especially projen which was just removed).
+          ...process.env.CI ? ['--frozen-lockfile'] : [],
+        ].join(' ');
+
+      case NodePackageManager.NPM:
+        return process.env.CI
+          ? 'npm ci'
+          : 'npm install';
+
+      default:
+        throw new Error(`unexpected package manager ${this.packageManager}`);
+    }
   }
 
   private loadDependencies(outdir: string) {
@@ -1228,10 +1278,10 @@ export class NodeBuildWorkflow extends GithubWorkflow {
         },
 
         // if there are changes, creates a bump commit
-        ...options.bump ? [{ run: 'yarn bump' }] : [],
+        ...options.bump ? [{ run: `${project.runScriptCommand} bump` }] : [],
 
         // build (compile + test)
-        { run: 'yarn build' },
+        { run: `${project.runScriptCommand} build` },
 
         // anti-tamper check (fails if there were changes to committed files)
         // this will identify any non-commited files generated during build (e.g. test snapshots)

src/typescript.ts
@@ -142,10 +142,11 @@ export class TypeScriptProject extends NodeProject {
 
     // by default, we first run tests (jest compiles the typescript in the background) and only then we compile.
     const compileBeforeTest = options.compileBeforeTest ?? false;
+
     if (compileBeforeTest) {
-      this.addBuildCommand('yarn compile', 'yarn test');
+      this.addBuildCommand(`${this.runScriptCommand} compile`, `${this.runScriptCommand} test`);
     } else {
-      this.addBuildCommand('yarn test', 'yarn compile');
+      this.addBuildCommand(`${this.runScriptCommand} test`, `${this.runScriptCommand} compile`);
     }
     this.start?.addEntry('build', {
       desc: 'Full release build (test+compile)',
@@ -156,11 +157,11 @@ export class TypeScriptProject extends NodeProject {
       this.addScript('package',
         'rm -fr dist',
         'mkdir -p dist/js',
-        'yarn pack',
+        `${this.runScriptCommand} pack`,
         'mv *.tgz dist/js/',
       );
 
-      this.addBuildCommand('yarn run package');
+      this.addBuildCommand(`${this.runScriptCommand} package`);
 
       this.start?.addEntry('package', {
         desc: 'Create an npm tarball',

.github/workflows/build.yml
@@ -20,7 +20,7 @@ jobs:
         run: |-
           git config user.name "Auto-bump"
           git config user.email "github-actions@github.com"
-      - run: yarn build
+      - run: yarn run build
       - name: Anti-tamper check
         run: git diff --exit-code
     container:

.github/workflows/release.yml
@@ -22,8 +22,8 @@ jobs:
         run: |-
           git config user.name "Auto-bump"
           git config user.email "github-actions@github.com"
-      - run: yarn bump
-      - run: yarn build
+      - run: yarn run bump
+      - run: yarn run build
       - name: Anti-tamper check
         run: git diff --exit-code
       - run: git push --follow-tags origin $GITHUB_REF

src/awscdk-app-ts.ts
@@ -119,7 +119,7 @@ export class AwsCdkTypeScriptApp extends TypeScriptAppProject {
 
     this.addScript('compile', 'true');
     this.removeScript('watch'); // because we use ts-node
-    this.addBuildCommand('yarn synth');
+    this.addBuildCommand(`${this.runScriptCommand} synth`);
 
     this.start?.addEntry('synth', {
       desc: 'Synthesizes your cdk app into cdk.out (part of "yarn build")',

src/eslint.ts
@@ -45,7 +45,7 @@ export class Eslint extends Component {
       desc: 'Runs eslint against the codebase',
       category: StartEntryCategory.TEST,
     });
-    project.addTestCommand('yarn eslint');
+    project.addTestCommand(`${project.runScriptCommand} eslint`);
 
     // exclude some files
     project.npmignore?.exclude('/.eslintrc.json');

src/jsii-project.ts
@@ -260,7 +260,7 @@ export class JsiiProject extends TypeScriptProject {
 
     const compat = options.compat ?? false;
     if (compat) {
-      this.addCompileCommand('yarn compat');
+      this.addCompileCommand(`${this.runScriptCommand} compat`);
     } else {
       this.addTip('Set "compat" to "true" to enable automatic API breaking-change validation');
     }

src/projen-upgrade.ts
@@ -65,7 +65,7 @@ export class ProjenUpgrade {
             ...project.workflowBootstrapSteps,
 
             // upgrade
-            { run: `yarn ${script}` },
+            { run: `${project.runScriptCommand} ${script}` },
 
             // submit a PR
             {

commit 52f8b332c3c2433ca35b8d4fd0a6a0651c8de0d3

fix(typescript): "package" script fails with "Command "pack" not found."
Bug was introduced in #157 which replaced all invocations of `yarn XXX` with `yarn run XXX`. However, the `pack` command used by the package script should not invoked through `npm/yarn run`.

src/node-project.ts
@@ -24,12 +24,12 @@ export enum NodePackageManager {
   /**
    * Use `yarn` as the package manager.
    */
-  YARN,
+  YARN = 'yarn',
 
   /**
    * Use `npm` as the package manager.
    */
-  NPM
+  NPM = 'npm'
 }
 
 export interface NodeProjectCommonOptions {

src/typescript.ts
@@ -157,7 +157,7 @@ export class TypeScriptProject extends NodeProject {
       this.addScript('package',
         'rm -fr dist',
         'mkdir -p dist/js',
-        `${this.runScriptCommand} pack`,
+        `${this.packageManager} pack`,
         'mv *.tgz dist/js/',
       );
 