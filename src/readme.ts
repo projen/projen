1c1b6145889a399eb0bdf3725a757bd04d4b7d2a
Matthew Bonig <matthew.bonig@gmail.com>
2020-10-18 15:41:22 -0600

    adding a README file to the jsii construct.

---
+++ src/readme.ts
@@ -0,0 +1,28 @@
+import {FileBase, FileBaseOptions, IResolver} from "./file";
+import {Project} from "./project";
+
+export interface ReadmeOptions extends FileBaseOptions {
+  /**
+   * The content of the README file.
+   *
+   * @default "# jsii construct"
+   */
+  readonly text?: string;
+}
+
+
+/**
+ * Represents a README.md file
+ */
+export class Readme extends FileBase {
+  private readonly text: string;
+
+  constructor(project: Project, options: ReadmeOptions = {}) {
+    super(project, "README.md", options);
+    this.text = options.text ?? "# jsii construct";
+  }
+
+  protected synthesizeContent(resolver: IResolver): string {
+    return resolver.resolve(this.text);
+  }
+}
