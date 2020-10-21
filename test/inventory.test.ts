fabe3a5d5595a4722442122600dba496b31c2811
Ken Winner <ken@getvoxi.com>
2020-10-21 12:54:29 -0400

    Add a test to simulate remote discovery in inventory

--- test/inventory.test.ts
+++ test/inventory.test.ts
@@ -1,6 +1,9 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
 import * as inventory from '../src/inventory';
 
 const result = inventory.discover();
+const simulatedExternalJsiiTypes: { [name: string]: inventory.JsiiType } = fs.readJsonSync(path.join(__dirname, '..', '.jsii')).types;
 
 test('project id', () => {
   expect(result.map(x => x.pjid).sort()).toContain('jsii');
@@ -12,3 +15,9 @@ test('inventory', () => {
   expect(result).toMatchSnapshot();
 });
 
+test('remote discover project id simulation', () => {
+  const remoteDiscoverResult = inventory.discover(simulatedExternalJsiiTypes);
+  expect(remoteDiscoverResult.map(x => x.pjid).sort()).toContain('jsii');
+  expect(remoteDiscoverResult.map(x => x.pjid).sort()).toContain('awscdk-construct');
+  expect(remoteDiscoverResult.map(x => x.pjid).sort()).toContain('typescript');
+});
