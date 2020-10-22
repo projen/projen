eb5f30b567cc24a4e3eed333e07cad0dc39aa3de
Joseph Egan <joseph.s.egan@gmail.com>
2020-10-22 01:47:21 +0100

    chore(makefile): add docstrings

--- src/makefile.ts
+++ src/makefile.ts
@@ -1,20 +1,62 @@
 import { FileBase, FileBaseOptions, IResolver } from './file';
 import { Project } from './project';
 
+/**
+ * A Make rule.
+ */
 export interface Rule {
+  /**
+   * Files to be created or updated by this rule.
+   *
+   * If the rule is phony then instead this represents the command's name(s).
+   */
   targets: string[];
+
+  /**
+   * Files that are used as inputs to create a target.
+   */
   prerequisites?: string[];
+
+  /**
+   * Commands that are run (using prerequisites as inputs) to create a target.
+   */
   recipe?: string[];
+
+  /**
+   * Marks whether the target is phony.
+   *
+   * False by default.
+   */
   phony?: boolean;
 }
 
+/**
+ * Options for Makefiles.
+ */
 export interface MakefileOptions extends FileBaseOptions {
-  all?: string[];
-  rules: Rule[];
+  /**
+   * List of targets to build when Make is invoked without specifying any targets.
+   */
+  readonly all?: string[];
+
+  /**
+   * Rules to include in the Makefile.
+   */
+  readonly rules: Rule[];
 }
 
+/**
+ * Minimal Makefile.
+ */
 export class Makefile extends FileBase {
+  /**
+   * Targets to make by default.
+   */
   public readonly all?: string[];
+
+  /**
+   * List of rule definitions.
+   */
   public readonly rules: Rule[];
 
 
