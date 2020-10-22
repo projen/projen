16fcc944e541d9b27014f5e0be2c227c23429980
Matthew Bonig <matthew.bonig@gmail.com>
2020-10-22 17:10:00 -0600

    moving SampleFile to file.ts and other cleanup

--- src/readme.ts
+++ src/readme.ts
@@ -1,16 +1,5 @@
-import { FileBaseOptions } from './file';
+import { SampleFile } from './file';
 import { Project } from './project';
-import { SampleFile } from './typescript';
-
-export interface ReadmeOptions extends FileBaseOptions {
-  /**
-   * The initial content of the README file.
-   *
-   * @default "# replace this"
-   */
-  readonly text?: string;
-}
-
 
 /**
  * Represents a README.md file. Unlike most other files this will only be created if the file doesn't already exist.
