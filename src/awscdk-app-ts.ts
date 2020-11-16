ed532aeb4a7a79d5445d09385160238e5eacfe05
Josh Kellendonk <joshkellendonk@gmail.com>
2020-11-16 10:50:21 -0700

    fix: cdk app test code syntax error

--- src/awscdk-app-ts.ts
+++ src/awscdk-app-ts.ts
@@ -225,8 +225,8 @@ app.synth();`;
     }
 
     const testCode = `import '@aws-cdk/assert/jest';
-import { MyStack } from '../src/main'
 import { App } from '@aws-cdk/core';
+import { MyStack } from '../src/main';
 
 test('Snapshot', () => {
   const app = new App();
