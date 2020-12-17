5af18db8aef84c0e96832b18e6864701bee747d5
Christopher Rybicki <crybicki98@gmail.com>
2020-12-17 03:33:24 -0500

    fix: tasks not executing on Windows due to invalid PATH values (#377)
    Fixes #370

--- projen.bash
+++ projen.bash
@@ -3,6 +3,6 @@
 set -euo pipefail
 if [ ! -f lib/cli/index.js ]; then
   echo "compiling the cli..."
-  ( PATH="$(npx -c 'echo $PATH')";  ( jsii --silence-warnings=reserved-word --no-fix-peer-dependencies ) && ( ( ( PATH="$(npx -c 'echo $PATH')";  ( jsii-docgen ) ) ) ) )
+  ( PATH="$(npx node -e "console.log(process.env.PATH)")";  ( jsii --silence-warnings=reserved-word --no-fix-peer-dependencies ) && ( ( ( PATH="$(npx node -e "console.log(process.env.PATH)")";  ( jsii-docgen ) ) ) ) )
 fi
 exec bin/projen $@
