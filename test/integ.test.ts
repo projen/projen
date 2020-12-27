86027bf4b933bd1842408a0cae7eb37eaf05db93
Elad Ben-Israel <benisrae@amazon.com>
2020-12-27 15:41:03 +0200

    chore: projen release workflow broken
    Since the integration test snapshots include the synthesized package.json, which includes the projen
    version itself as a devDependency, when projen itself is bumped as part of the projen release, the snapshot fails.
    
    To fix this, we replace the version of the `projen` devDependency in the synthesized `package.json` file in with `999.999.999`.
    
    Fixes #410

--- test/integ.test.ts
+++ test/integ.test.ts
@@ -1,9 +1,11 @@
 import { join, dirname, basename } from 'path';
-import { copySync } from 'fs-extra';
+import { copySync, readJsonSync, writeJsonSync } from 'fs-extra';
 import { glob } from 'glob';
 import { exec } from '../src/util';
 import { mkdtemp, directorySnapshot } from './util';
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const projenVersion = require('../package.json').version;
 const cli = require.resolve('../lib/cli');
 const samples = join(__dirname, 'integration');
 const files = glob.sync('**/*.projenrc.js', { cwd: samples });
@@ -22,6 +24,25 @@ for (const projenrc of files) {
       '--no-post',
     ];
     exec(command.join(' '), { cwd: workdir });
+
+    // patch the projen version in package.json to match the current version
+    // otherwise, every bump would need to update these snapshots.
+    patchPacakgeJson(workdir);
+
     expect(directorySnapshot(workdir, { excludeGlobs: ['node_modules/**'] })).toMatchSnapshot();
   });
+}
+
+function patchPacakgeJson(dir: string) {
+  const filepath = join(dir, 'package.json');
+  const pkg = readJsonSync(filepath);
+  const prev = pkg.devDependencies.projen;
+  if (!prev) {
+    throw new Error(`expecting "${filepath}" to include a devDependency on "projen"`);
+  }
+
+  // replace the current projen version with 999.999.999 for deterministic output.
+  // this will preserve any semantic version requirements (e.g. "^", "~", etc).
+  pkg.devDependencies.projen = prev.replace(projenVersion, '999.999.999');
+  writeJsonSync(filepath, pkg);
 }
