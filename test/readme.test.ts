7a380784dc9f8fc5c68ea509211e2b48a2c34af1
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-30 19:51:01 -0500

    start tests

--- test/readme.test.ts
+++ test/readme.test.ts
@@ -3,21 +3,7 @@ import { synthSnapshot, TestProject } from './util';
 
 logging.disable();
 
-describe('Readme', () => {
-  test('default Readme', () => {
-    // GIVEN
-    const project = new TestProject();
-
-    // WHEN
-    const output = synthSnapshot(project);
-    console.log(output);
-    // THEN
-    const upper = output['README.md'];
-    const lower = output['readme.md'];
-    expect(upper).toBeTruthy();
-    expect(lower).toBeFalsy();
-  });
-
+describe('Readme Enablement', () => {
   test('Readme is off', () => {
     // GIVEN
     const project = new TestProject({
@@ -32,5 +18,56 @@ describe('Readme', () => {
     const lower = output['readme.md'];
     expect(upper).toBeFalsy();
     expect(lower).toBeFalsy();
+  });
+
+  test('default Readme is on', () => {
+    // GIVEN
+    const project = new TestProject();
+
+    // WHEN
+    const output = synthSnapshot(project);
+    console.log(output);
+    // THEN
+    const upper = output['README.md'];
+    const lower = output['readme.md'];
+    expect(upper).toBeTruthy();
+    expect(lower).toBeFalsy();
+    // expect match
+  });
+});
+
+describe('Readme customization', () => {
+  test('Readme has custom filename', () => {
+    // GIVEN
+    const project = new TestProject({
+      readmeConfig: {
+        filename: 'readme.md',
+      },
+    });
+
+    // WHEN
+    const output = synthSnapshot(project);
+
+    // THEN
+    const upper = output['README.md'];
+    const lower = output['readme.md'];
+    expect(upper).toBeFalsy();
+    expect(lower).toBeTruthy();
+  });
+
+  test('Readme has custom sections', () => {
+    // GIVEN
+    const project = new TestProject({
+      readmeConfig: {
+        tagLine: 'test tag line',
+      },
+    });
+
+    // WHEN
+    const output = synthSnapshot(project);
+
+    // THEN
+    const readme = output['README.md'];
+    expect(readme).toContain('test tag line');
   });
 });
