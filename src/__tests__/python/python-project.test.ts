145a509281091d5d3fdade3ec44df286601b8c86
Chriscbr <crybicki98@gmail.com>
2021-01-23 03:23:00 -0500

    add more tests

--- src/__tests__/python/python-project.test.ts
+++ src/__tests__/python/python-project.test.ts
@@ -7,10 +7,40 @@ test('defaults', () => {
   expect(synthSnapshot(p)).toMatchSnapshot();
 });
 
+test('dependencies', () => {
+  const p = new TestPythonProject();
+  p.addDependency('Django@3.1.5');
+  p.addDependency('aws-cdk.core@*');
+  p.addTestDependency('hypothesis@^6.0.3');
+  expect(synthSnapshot(p)).toMatchSnapshot();
+});
+
+test('dependencies via ctor', () => {
+  const p = new TestPythonProject({
+    deps: [
+      'Django@3.1.5',
+      'aws-cdk.core@*',
+    ],
+    testDeps: [
+      'hypothesis@^6.0.3',
+    ],
+  });
+  expect(synthSnapshot(p)).toMatchSnapshot();
+});
+
+test('no pytest', () => {
+  const p = new TestPythonProject({
+    pytest: false,
+  });
+
+  expect(synthSnapshot(p)).toMatchSnapshot();
+});
+
 class TestPythonProject extends PythonProject {
   constructor(options: Partial<PythonProjectOptions> = { }) {
     super({
       ...options,
+      clobber: false,
       name: 'test-python-project',
       outdir: mkdtemp(),
       logging: { level: LogLevel.OFF },
