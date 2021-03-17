5c81e80ffa933629dd1fd64def64ac3a9f6e435c
Elad Ben-Israel <benisrae@amazon.com>
2021-03-17 16:56:31 +0200

    chore: update description
    Updating package description (and testing mutable builds along the way)

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const { JsiiProject, JsonFile, TextFile } = require('./lib');
 
 const project = new JsiiProject({
   name: 'projen',
-  description: 'A new generation of project generators',
+  description: 'CDK for software projects',
   repository: 'https://github.com/projen/projen.git',
   authorName: 'Elad Ben-Israel',
   authorEmail: 'benisrae@amazon.com',
