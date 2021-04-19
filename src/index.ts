fc274291ea83bfa6b62d443eeb80235bc3fc20cd
Aatman <aatman@auroville.org.in>
2021-04-19 17:15:16 +0530

    feat: cdk8s-app-ts  (#699)
    REF: #694
    
    Steps to test :
    ```
    1. yarn projen new cdk8s-app-ts
    2. yarn build
    ```
    TODO:
    
    - [ ] Sample code for tests
    - [x] Unit tests
    
    ---
    
    Extending from TypeScriptApp. Release and Build workflows have synth included.
    
    Might have forgotten a few things, feel free to add them in the comments below : )
    
    ---
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './awscdk-app-ts';
 export * from './awscdk-construct';
+export * from './cdk8s-app-ts';
 export * from './cdk8s-construct';
 export * from './component';
 export * from './construct-lib';
