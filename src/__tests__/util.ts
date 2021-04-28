e61882dfc59e805e0922dbe8968ccf8f9555b68a
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-04-28 15:04:09 +0200

    correctly do the cleanup

--- src/__tests__/util.ts
+++ src/__tests__/util.ts
@@ -43,8 +43,29 @@ export interface SynthOutput {
   [filePath: string]: any;
 }
 
+const autoRemove = new Set<string>();
+
+// Hook to automatically remove temporary directories without needing each
+// place to actually handle this specifically.
+afterAll((done) => {
+  // Array.from used to get a copy, so we can safely remove from the set
+  for (const dir of Array.from(autoRemove)) {
+    try {
+      // Note - fs-extra.removeSync is idempotent, so we're safe if the
+      // directory has already been cleaned up before we get there!
+      fs.removeSync(dir);
+    } catch (e) {
+      done.fail(e);
+    }
+    autoRemove.delete(dir);
+  }
+  done();
+});
+
 export function mkdtemp() {
-  return removeAtExit(fs.mkdtempSync(path.join(os.tmpdir(), 'projen-test-')));
+  const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'projen-test-'));
+  autoRemove.add(tmpdir);
+  return tmpdir;
 }
 
 export function synthSnapshot(project: Project): any {
@@ -106,21 +127,4 @@ export function directorySnapshot(root: string, options: DirectorySnapshotOption
   }
 
   return output;
-}
-
-const cleanUpDirectories = new Set<string>();
-function removeAtExit(directory: string): string {
-  if (cleanUpDirectories.size === 0) {
-    process.once('exit', () => {
-      for (const dir of cleanUpDirectories) {
-        try {
-          fs.removeSync(dir);
-        } catch {
-          // IGNORE: It's unsafe to do much else during `exit`, unfortunately.
-        }
-      }
-    });
-  }
-  cleanUpDirectories.add(directory);
-  return directory;
 }
