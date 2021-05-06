d024a777e058a85d3ef326d025167fd35c4d0855
Elad Ben-Israel <benisrae@amazon.com>
2021-05-06 16:00:15 +0300

    Update src/version.ts
    Co-authored-by: Romain Marcadier <rmuller@amazon.com>

--- src/version.ts
+++ src/version.ts
@@ -31,7 +31,7 @@ export class Version extends Component {
       '-c "versionsort.suffix=-"', // makes sure pre-release versions are listed after the primary version
       'tag',
       '--sort="-version:refname"', // sort as versions and not lexicographically
-      '--list "v*"', // only tags that start with "v"
+      '--list="v*"', // only tags that start with "v"
     ].join(' ');
 
     this.bumpTask.exec(`${listGitTags} | head -n1 > ${versionFile}`);
