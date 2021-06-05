7788eee8feb50aae78fa7a83210c09c5f42c67cb
Eli Polonsky <epolon@amazon.com>
2021-06-05 19:31:31 +0300

    rewire webpack config

--- src/__tests__/web/react-project.test.ts
+++ src/__tests__/web/react-project.test.ts
@@ -7,6 +7,39 @@ test('defaults', () => {
   expect(synthSnapshot(p)).toMatchSnapshot();
 });
 
+test('rewire creates config-overrides.js', () => {
+  const p = new TestReactProject({
+    rewire: {
+      'module.prop1': false,
+      'module.prop2': 5,
+      'module.prop3': [1, 2],
+      'module.prop4': { one: 2 },
+      'module.prop5': 'one',
+    },
+  });
+  const snapshot = synthSnapshot(p)['config-overrides.js'];
+  expect(snapshot).toMatchSnapshot();
+});
+
+test('rewire replaces react-scripts', () => {
+  const p = new TestReactProject({
+    rewire: { 'module.prop1': false },
+  });
+
+  function assertExec(taskName: string, script: string) {
+    const task = p.tasks.tryFind(taskName);
+    if (!task) {
+      throw new Error(`Task not found: ${taskName}`);
+    }
+    expect(task.steps[0].exec).toEqual(script);
+  }
+
+  assertExec('build', 'react-app-rewired build');
+  assertExec('test', 'react-app-rewired test');
+  assertExec('dev', 'react-app-rewired start');
+  assertExec('eject', 'react-scripts eject');
+});
+
 class TestReactProject extends ReactProject {
   constructor(options: Partial<ReactProjectOptions> = { }) {
     super({
