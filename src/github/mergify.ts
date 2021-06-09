507755184acfb878f8e2eafd2a19cb8bf76a0e0c
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2021-06-09 14:13:25 +0200

    feat: add condition for mergify rule

--- src/github/mergify.ts
+++ src/github/mergify.ts
@@ -2,9 +2,34 @@ import { Component } from '../component';
 import { YamlFile } from '../yaml';
 import { GitHub } from './github';
 
+/**
+ * The Mergify conditional operators that can be used are: `or` and `and`.
+ * Note: The number of nested conditions is limited to 3.
+ */
+export interface MergifyConditionalOperator {
+  readonly or?: MergifyCondition[];
+  readonly and?: MergifyCondition[];
+}
+
+export type MergifyCondition = string | MergifyConditionalOperator;
+
 export interface MergifyRule {
+  /**
+   * The name of the rule. This is not used by the engine directly,
+   * but is used when reporting information about a rule.
+   */
   readonly name: string;
-  readonly conditions: string[];
+  /**
+   * A list of Conditions string that must match against the
+   * pull request for the rule to be applied.
+   * @see https://docs.mergify.io/conditions/#combining-conditions-with-operators
+   */
+  readonly conditions: MergifyCondition[];
+  /**
+   * A dictionary made of Actions that will be executed on the
+   * matching pull requests.
+   * @see https://docs.mergify.io/actions/#actions
+   */
   readonly actions: { [action: string]: any };
 }
 
