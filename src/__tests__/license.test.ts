ad6d2f4865447af4115a9295499f585ef9ba9b3b
pvbouwel <petervanbouwel@gmail.com>
2021-08-09 13:47:04 +0200

    feat: MIT-0 license (#978)
    Add the MIT No Attribution (MIT-0) license.
    
    ---
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- src/__tests__/license.test.ts
+++ src/__tests__/license.test.ts
@@ -42,3 +42,33 @@ test('MIT with owner and period', () => {
   expect(synthSnapshot(project).LICENSE).toMatchSnapshot();
 });
 
+test('fails for MIT-0 with no owner because substitution is required', () => {
+  const project = new TestProject();
+
+  expect(() => new License(project, {
+    spdx: 'MIT-0',
+  })).toThrow(/The MIT-0 license requires \"copyrightOwner\" to be specified/);
+});
+
+test('MIT-0 with owner', () => {
+  const project = new TestProject();
+
+  new License(project, {
+    spdx: 'MIT-0',
+    copyrightOwner: 'John Doe',
+  });
+
+  expect(synthSnapshot(project).LICENSE).toMatchSnapshot();
+});
+
+test('MIT-0 with owner and period', () => {
+  const project = new TestProject();
+
+  new License(project, {
+    spdx: 'MIT-0',
+    copyrightOwner: 'John Doe',
+    copyrightPeriod: '1900-1920',
+  });
+
+  expect(synthSnapshot(project).LICENSE).toMatchSnapshot();
+});
