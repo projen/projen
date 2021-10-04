b493fac6182e93235b40e7a028d37ac4929af14d
Elad Ben-Israel <benisrae@amazon.com>
2021-10-04 18:33:16 +0300

    feat: bump minimum node version to 12.x (#1113)

--- .projenrc.js
+++ .projenrc.js
@@ -41,7 +41,7 @@ const project = new JsiiProject({
 
   projenDevDependency: false, // because I am projen
   releaseToNpm: true,
-  minNodeVersion: '10.17.0',
+  minNodeVersion: '12.7.0',
   codeCov: true,
   defaultReleaseBranch: 'main',
   gitpod: true,
