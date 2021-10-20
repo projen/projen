8afadc7a105f33a48c067f77339d0b878d307e35
Matthew Bonig <matthew.bonig@gmail.com>
2021-10-20 09:57:45 -0600

    adding example return schema

--- src/testing.ts
+++ src/testing.ts
@@ -12,6 +12,7 @@ export class Testing {
   /**
    * Produces a simple JS object that represents the contents of the projects with field names being file paths.
    * @param project the project to produce a snapshot for
+   * @return { [filename:string]: any }
    */
   public static synth(project: Project): Record<string, any> {
     return synthSnapshot(project);
