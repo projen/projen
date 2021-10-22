51559631bedeccc439cb02f3582f3ea33cf8d744
Alexander Forsyth <alex@alexforsyth.io>
2021-10-22 16:12:10 -0400

    Update project.ts

--- src/project.ts
+++ src/project.ts
@@ -458,7 +458,7 @@ export class Project {
       return mkdtempSync(path.join(tmpdir(), 'projen.'));
     }
 
-    return path.resolve(this.outdir || '.');
+    return path.resolve(outdirOption || '.');
   }
 }
 
