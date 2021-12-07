dd304002449ddd77cb7f4d43aa358024e4b68028
Elad Ben-Israel <benisrae@amazon.com>
2021-12-07 20:55:00 +0200

    fix(awscdk): integ tests are still not working for libraries
    Fixes #1300
    Fixes #1299 (again)

--- test/awscdk/integration-test.test.ts
+++ test/awscdk/integration-test.test.ts
@@ -1,5 +1,7 @@
 import { awscdk } from '../../src';
+import { IntegrationTest } from '../../src/awscdk';
 import { Testing } from '../../src/testing';
+import { TypeScriptProject } from '../../src/typescript';
 
 describe('IntegrationTest', () => {
   // GIVEN
@@ -58,5 +60,22 @@ describe('IntegrationTest', () => {
       expect(actualTaskNames).toContain(t);
       expect(output['.projen/tasks.json'].tasks[t]).toMatchSnapshot();
     }
+  });
+});
+
+test('installs ts-node if needed', () => {
+  const project = new TypeScriptProject({
+    name: 'test',
+    defaultReleaseBranch: 'main',
+  });
+
+  new IntegrationTest(project, {
+    entrypoint: 'test/foo.integ.ts',
+    tsconfigPath: project.tsconfigDev.fileName,
+  });
+
+  expect(project.deps.getDependency('ts-node')).toStrictEqual({
+    name: 'ts-node',
+    type: 'build',
   });
 });
