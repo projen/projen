daf0a076f147969a55dcb493f939c9d4b64571f5
Elad Ben-Israel <benisrae@amazon.com>
2021-12-20 02:34:44 +0200

    feat: allow overriding deps (#1351)
    It is now possible to add a dependency to a project with a different version specification (at your own risk). Last one prevails.
    
    Fixes #1342
    
    ---
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- test/web/react-ts-project.test.ts
+++ test/web/react-ts-project.test.ts
@@ -6,6 +6,25 @@ test('defaults', () => {
   expect(synthSnapshot(p)).toMatchSnapshot();
 });
 
+// see https://github.com/projen/projen/issues/1342
+test('deps can be overridden', () => {
+  const p = new TestReactTypeScriptProject();
+
+  p.deps.removeDependency('react');
+  p.deps.removeDependency('react-dom');
+  p.deps.removeDependency('react-scripts');
+  p.deps.removeDependency('web-vitals');
+  p.addDevDeps('react@^2', 'react-dom@^4', 'react-scripts@1.2.3', 'web-vitals@7.2');
+
+  const pkg = synthSnapshot(p)['package.json'];
+
+  expect(pkg.dependencies).toStrictEqual({});
+  expect(pkg.devDependencies.react).toStrictEqual('^2');
+  expect(pkg.devDependencies['react-dom']).toStrictEqual('^4');
+  expect(pkg.devDependencies['react-scripts']).toStrictEqual('1.2.3');
+  expect(pkg.devDependencies['web-vitals']).toStrictEqual('7.2');
+});
+
 class TestReactTypeScriptProject extends ReactTypeScriptProject {
   constructor(options: Partial<ReactTypeScriptProjectOptions> = { }) {
     super({
