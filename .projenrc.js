2e99bf21548beddab92af828cd89f270632666cd
Elad Ben-Israel <benisrae@amazon.com>
2021-12-26 13:20:01 +0200

    chore: enable publishing (#1366)
    ---
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- .projenrc.js
+++ .projenrc.js
@@ -68,8 +68,6 @@ const project = new cdk.JsiiProject({
     coverageText: false,
   },
 
-  publishDryRun: true,
-
   publishToMaven: {
     javaPackage: 'io.github.cdklabs.projen',
     mavenGroupId: 'io.github.cdklabs',
