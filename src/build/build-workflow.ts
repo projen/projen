79cc132abccf2778a0ca652e5bc22e88329b0040
epolon <epolon@amazon.com>
2021-12-27 19:15:03 +0200

    only add node_modules if its a node project

--- src/build/build-workflow.ts
+++ src/build/build-workflow.ts
@@ -3,6 +3,7 @@ import { Component } from '../component';
 import { GitHub, GithubWorkflow, GitIdentity } from '../github';
 import { DEFAULT_GITHUB_ACTIONS_USER, setGitIdentityStep } from '../github/constants';
 import { Job, JobPermission, JobStep } from '../github/workflows-model';
+import { NodeProject } from '../javascript';
 import { Project } from '../project';
 
 const BRANCH_REF = '${{ github.event.pull_request.head.ref }}';
@@ -218,6 +219,17 @@ export class BuildWorkflow extends Component {
       // add a step at the end of the build workflow which will upload the
       // artifact so we can download it in each post-build job (do it once).
       if (!this.uploadArtitactSteps) {
+
+        const paths = ['.', '!.git'];
+
+        if (this.project instanceof NodeProject) {
+          paths.push(
+            // node_modules takes forever to compress.
+            // instead, we skip it and re-install after downloading.
+            '!node_modules',
+          );
+        }
+
         this.uploadArtitactSteps = [{
           name: 'Upload artifact',
           uses: 'actions/upload-artifact@v2.1.1',
@@ -226,13 +238,7 @@ export class BuildWorkflow extends Component {
           if: 'always()',
           with: {
             name: artfiactName,
-            path: [
-              '.',
-              '!.git',
-              // node_modules takes forever to compress.
-              // instead we skip it and re-install after downloading.
-              '!node_modules',
-            ].join('\n'),
+            path: paths.join('\n'),
           },
         }];
       }
