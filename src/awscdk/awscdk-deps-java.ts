7353bb0c0cd4d7a4873a61403e8ce6d60109afe3
Matteo Sessa <matteo.sessa@service.vic.gov.au>
2022-01-02 00:03:41 +1100

    chore: fix comment, rename variable for clarity

--- src/awscdk/awscdk-deps-java.ts
+++ src/awscdk/awscdk-deps-java.ts
@@ -2,16 +2,16 @@ import { Project } from '../project';
 import { AwsCdkDeps, AwsCdkDepsOptions } from './awscdk-deps';
 
 /**
- * Manages dependencies on the AWS CDK for Node.js projects.
+ * Manages dependencies on the AWS CDK for Java projects.
  */
 export class AwsCdkDepsJava extends AwsCdkDeps {
   static readonly PACKAGE_GROUP = 'software.amazon.awscdk';
 
   constructor(project: Project, options: AwsCdkDepsOptions) {
     // Ensure cdkDependencies are passed as fully qualified form to the super class
-    const depsFixup = options.cdkDependencies?.map((dep) => dep.includes('/') ? dep : `${AwsCdkDepsJava.PACKAGE_GROUP}/${dep}`);
+    const fqCdkDeps = options.cdkDependencies?.map((dep) => dep.includes('/') ? dep : `${AwsCdkDepsJava.PACKAGE_GROUP}/${dep}`);
 
-    super(project, { ...options, cdkDependencies: depsFixup }
+    super(project, { ...options, cdkDependencies: fqCdkDeps }
       , {
         frameworkV1Package: `${AwsCdkDepsJava.PACKAGE_GROUP}/core`,
         frameworkV2Package: `${AwsCdkDepsJava.PACKAGE_GROUP}/aws-cdk-lib`,
