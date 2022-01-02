2ef39f895b255dc4c844947f29fb0568eb6f4088
Arun Donti <dontirun@gmail.com>
2022-01-02 16:46:05 -0500

    feat: GitLab CI model and base classes (#1357)
    Related to #137, #942
    
    This PR adds the model and some opinionated base classes for GitLab CI configurations (which should be reviewed). This currently does not tie the classes into a project configuration. I have not added any tests, but would be happy to add them as this PR progresses.
    
    Base schema from [here](https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json)
    Initial TypeScript Interfaces (before opinionated modifications) generated from [here](https://app.quicktype.io/#)
    
    ---
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

---
+++ test/gitlab/configuration.test.ts
@@ -0,0 +1,94 @@
+import { CiConfiguration } from "../../src/gitlab";
+import { TestProject } from "../util";
+
+test("throws when adding an existing service with same name and alias", () => {
+  // GIVEN
+  const p = new TestProject({
+    stale: true,
+  });
+  const c = new CiConfiguration(p, "foo");
+  c.addServices({ name: "bar" });
+  // THEN
+  expect(() => c.addServices({ name: "bar" })).toThrowError(
+    /GitLab CI already contains/
+  );
+  expect(() =>
+    c.addServices({ name: "foobar" }, { name: "foobar" })
+  ).toThrowError(/GitLab CI already contains/);
+});
+
+test("does not throw when adding an services with same name and different alias", () => {
+  // GIVEN
+  const p = new TestProject({
+    stale: true,
+  });
+  const c = new CiConfiguration(p, "foo");
+  c.addServices({ name: "foo", alias: "foobar" });
+  // THEN
+  expect(() => c.addServices({ name: "foo", alias: "baz" })).not.toThrowError(
+    /GitLab CI already contains/
+  );
+});
+
+test("does not throw when adding an valid include", () => {
+  // GIVEN
+  const p = new TestProject({
+    stale: true,
+  });
+  const c = new CiConfiguration(p, "foo");
+  // THEN
+  expect(() => c.addIncludes({ local: "foo" })).not.toThrowError(
+    /A valid include configuration specifies/
+  );
+  expect(() =>
+    c.addIncludes({ file: ["foo"], project: "foo" })
+  ).not.toThrowError(/A valid include configuration specifies/);
+  expect(() => c.addIncludes({ remote: "foo" })).not.toThrowError(
+    /A valid include configuration specifies/
+  );
+  expect(() => c.addIncludes({ template: "foo" })).not.toThrowError(
+    /A valid include configuration specifies/
+  );
+});
+
+test("throws when adding an invalid include", () => {
+  // GIVEN
+  const p = new TestProject({
+    stale: true,
+  });
+  const c = new CiConfiguration(p, "foo");
+  // THEN
+  expect(() =>
+    c.addIncludes({ file: ["foo"], project: "foo", local: "foo" })
+  ).toThrow(/contains 2 property combination/);
+  expect(() => c.addIncludes({ project: "foo" })).toThrow(
+    /contains 0 property combination/
+  );
+});
+
+test("throws when adding an existing includes", () => {
+  // GIVEN
+  const p = new TestProject({
+    stale: true,
+  });
+  const c = new CiConfiguration(p, "foo");
+  c.addIncludes(
+    { local: "foo" },
+    { file: ["foo"], project: "foo" },
+    { remote: "foo" },
+    { template: "foo" }
+  );
+  // THEN
+  expect(() => c.addIncludes({ local: "foo" })).toThrowError(
+    /already contains one or more templates specified in/
+  );
+  expect(() => c.addIncludes({ file: ["foo"], project: "foo" })).toThrowError(
+    /already contains one or more templates specified in/
+  );
+  expect(() => c.addIncludes({ remote: "foo" })).toThrowError(
+    /already contains one or more templates specified in/
+  );
+  expect(() => c.addIncludes({ template: "foo" })).toThrowError(
+    /already contains one or more templates specified in/
+  );
+});
