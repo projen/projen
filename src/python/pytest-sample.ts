928f12235d3793e11b0dc5df9377fcba5ee149ba
Marcio Cruz de Almeida <marciocadev@gmail.com>
2022-01-29 20:08:59 -0300

    feat(python): decoupling of test sample from pytest class into pytest-sample class, that way I can do specific tests when I create a python cdk app

---
+++ src/python/pytest-sample.ts
@@ -0,0 +1,32 @@
+import { PythonProject } from ".";
+import { Component, SampleDir } from "..";
+
+export class PytestSample extends Component {
+  constructor(project: PythonProject, testdir: string) {
+    super(project);
+
+    new SampleDir(project, testdir, {
+      files: {
+        "__init__.py": "",
+        "test_example.py": [
+          "import pytest",
+          "",
+          `from ${project.moduleName}.example import hello`,
+          "",
+          "@pytest.mark.parametrize(",
+          '    ("name", "expected"),',
+          "    [",
+          '        ("A. Musing", "Hello A. Musing!"),',
+          '        ("traveler", "Hello traveler!"),',
+          '        ("projen developer", "Hello projen developer!"),',
+          "    ],",
+          ")",
+          "def test_hello(name, expected):",
+          '    """Example test with parametrization."""',
+          "    assert hello(name) == expected",
+          "",
+        ].join("\n"),
+      },
+    });
+  }
+}
