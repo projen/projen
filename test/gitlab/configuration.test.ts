836ed2b096af85c4c13a3000b2e39f6cb6669944
Nicolas Byl <nicolas.byl@nexineer.io>
2022-04-27 20:06:48 +0200

    test(gitlab): use correct file names and update snapshot

--- test/gitlab/configuration.test.ts
+++ test/gitlab/configuration.test.ts
@@ -143,7 +143,7 @@ test("adds correct entries for path-based caching", () => {
   });
   const snapshot = synthSnapshot(p);
   // THEN
-  expect(snapshot[".gitlab-ci.yml"]).toMatchSnapshot();
+  expect(snapshot[".gitlab/ci-templates/foo.yml"]).toMatchSnapshot();
 });
 
 test("adds correct entries for file-based caching", () => {
@@ -163,5 +163,5 @@ test("adds correct entries for file-based caching", () => {
   });
   const snapshot = synthSnapshot(p);
   // THEN
-  expect(snapshot[".gitlab-ci.yml"]).toMatchSnapshot();
+  expect(snapshot[".gitlab/ci-templates/foo.yml"]).toMatchSnapshot();
 });
