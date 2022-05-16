dd6c68e8db890ed35143f0f6276f375443e19c87
Adrian Dimech <51220968+agdimech@users.noreply.github.com>
2022-05-16 15:25:33 +1000

    fix: resolve issue where generated import prefix is incorrect in .pro… (#1851)
    I am seeing an error whereby the generated .projenrc.py does not have the correct module_name. The FQN is aws-prototyping-sdk... however the exported python moduleName is aws_prototyping_sdk.
    
    This PR ensures that the declared module_name takes precedence.
    
    Repro steps:
    
    ```
    mkdir foo && cd foo
    npx projen new --from aws-prototyping-sdk pdk-pipeline-py
    npx projen # this will throw the error
    ```
    
    ---
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- src/python/projenrc.ts
+++ src/python/projenrc.ts
@@ -98,7 +98,9 @@ export class Projenrc extends Component {
       bootstrap.args
     );
 
-    emit(toPythonImport(jsiiFqn));
+    const importName = resolvePythonImportName(jsiiFqn, jsiiManifest);
+    emit(toPythonImport(importName));
+
     for (const fqn of imports) {
       emit(toPythonImport(fqn));
     }
@@ -114,6 +116,15 @@ export class Projenrc extends Component {
       `Project definition file was created at ${pythonFile}`
     );
   }
+}
+
+export function resolvePythonImportName(jsiiFqn: string, jsiiManifest: any) {
+  const moduleName = jsiiManifest?.targets?.python?.module;
+
+  // Module name prefix should take precedence in the event moduleName !== fqn prefix
+  return moduleName
+    ? [moduleName, ...jsiiFqn.split(".").slice(1)].join(".")
+    : jsiiFqn;
 }
 
 function renderPythonOptions(
