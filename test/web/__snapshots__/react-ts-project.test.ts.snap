projen/projen

commit 41c95b73c06ef71bfdd2d146ad9f6263bdbb6db8

fix(react): build errors with ReactTypescriptProject (#1911)
This PR fixes 2 build issues with the ReactTypescriptProject:

1. react-app-env.d.ts was not being synthesized into the src directory. This PR ensures it is emitted into the src root whcih fixes the issue with svg imports.
2. Removed redundant React import from unit test as this was causing TSErrors for unused imports.

Fixes: #1635 
---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

src/web/react.ts
@@ -162,7 +162,10 @@ export class ReactTypeScriptProject extends TypeScriptAppProject {
 
     new ReactComponent(this, { typescript: true, rewire: options.rewire });
 
-    this.reactTypeDef = new ReactTypeDef(this, "react-app-env.d.ts");
+    this.reactTypeDef = new ReactTypeDef(
+      this,
+      `${this.srcdir}/react-app-env.d.ts`
+    );
 
     // generate sample code in `src` and `public` if these directories are empty or non-existent.
     if (options.sampleCode ?? true) {
@@ -374,7 +377,6 @@ class ReactSampleCode extends Component {
     ];
 
     const appTestJsx = [
-      "import React from 'react';",
       "import { render, screen } from '@testing-library/react';",
       "import App from './App';",
       "",

test/web/__snapshots__/react-project.test.ts.snap
@@ -1010,8 +1010,7 @@ function App() {
 
 export default App;
 ",
-  "src/App.test.jsx": "import React from 'react';
-import { render, screen } from '@testing-library/react';
+  "src/App.test.jsx": "import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {

test/web/__snapshots__/react-ts-project.test.ts.snap
@@ -18,7 +18,7 @@ Object {
 /.projen/tasks.json linguist-generated
 /LICENSE linguist-generated
 /package.json linguist-generated
-/react-app-env.d.ts linguist-generated
+/src/react-app-env.d.ts linguist-generated
 /tsconfig.dev.json linguist-generated
 /tsconfig.json linguist-generated
 /yarn.lock linguist-generated",
@@ -254,7 +254,7 @@ jspm_packages/
 /lib
 /dist/
 /build/
-!/react-app-env.d.ts
+!/src/react-app-env.d.ts
 ",
   ".mergify.yml": "# ~~ Generated by projen. To modify, edit .projenrc.js and run \\"npx projen\\".
 
@@ -378,7 +378,7 @@ tsconfig.tsbuildinfo
       ".projen/files.json",
       ".projen/tasks.json",
       "LICENSE",
-      "react-app-env.d.ts",
+      "src/react-app-env.d.ts",
       "tsconfig.dev.json",
       "tsconfig.json",
     ],
@@ -861,145 +861,7 @@ tsconfig.tsbuildinfo
 User-agent: *
 Disallow:
 ",
-  "react-app-env.d.ts": "/// <reference types=\\"react-scripts\\" />",
-  "src/App.css": ".App {
-  text-align: center;
-}
-
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-
-.App-header {
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}
-
-.App-link {
-  color: #61dafb;
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-",
-  "src/App.test.tsx": "import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-",
-  "src/App.tsx": "import logo from './logo.svg';
-import './App.css'
-
-function App() {
-  return (
-   <div className=\\"App\\">
-      <header className=\\"App-header\\">
-        <img src={logo} className=\\"App-logo\\" alt=\\"logo\\" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className=\\"App-link\\"
-          href=\\"https://reactjs.org\\"
-          target=\\"_blank\\"
-          rel=\\"noopener noreferrer\\"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-",
-  "src/index.css": "body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
-",
-  "src/index.tsx": "import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-",
-  "src/logo.svg": "<svg xmlns=\\"http://www.w3.org/2000/svg\\" viewBox=\\"0 0 841.9 595.3\\">
-    <g fill=\\"#61DAFB\\">
-        <path d=\\"M666.3 296.5c0-32.5-40.7-63.3-103.1-82.4 14.4-63.6 8-114.2-20.2-130.4-6.5-3.8-14.1-5.6-22.4-5.6v22.3c4.6 0 8.3.9 11.4 2.6 13.6 7.8 19.5 37.5 14.9 75.7-1.1 9.4-2.9 19.3-5.1 29.4-19.6-4.8-41-8.5-63.5-10.9-13.5-18.5-27.5-35.3-41.6-50 32.6-30.3 63.2-46.9 84-46.9V78c-27.5 0-63.5 19.6-99.9 53.6-36.4-33.8-72.4-53.2-99.9-53.2v22.3c20.7 0 51.4 16.5 84 46.6-14 14.7-28 31.4-41.3 49.9-22.6 2.4-44 6.1-63.6 11-2.3-10-4-19.7-5.2-29-4.7-38.2 1.1-67.9 14.6-75.8 3-1.8 6.9-2.6 11.5-2.6V78.5c-8.4 0-16 1.8-22.6 5.6-28.1 16.2-34.4 66.7-19.9 130.1-62.2 19.2-102.7 49.9-102.7 82.3 0 32.5 40.7 63.3 103.1 82.4-14.4 63.6-8 114.2 20.2 130.4 6.5 3.8 14.1 5.6 22.5 5.6 27.5 0 63.5-19.6 99.9-53.6 36.4 33.8 72.4 53.2 99.9 53.2 8.4 0 16-1.8 22.6-5.6 28.1-16.2 34.4-66.7 19.9-130.1 62-19.1 102.5-49.9 102.5-82.3zm-130.2-66.7c-3.7 12.9-8.3 26.2-13.5 39.5-4.1-8-8.4-16-13.1-24-4.6-8-9.5-15.8-14.4-23.4 14.2 2.1 27.9 4.7 41 7.9zm-45.8 106.5c-7.8 13.5-15.8 26.3-24.1 38.2-14.9 1.3-30 2-45.2 2-15.1 0-30.2-.7-45-1.9-8.3-11.9-16.4-24.6-24.2-38-7.6-13.1-14.5-26.4-20.8-39.8 6.2-13.4 13.2-26.8 20.7-39.9 7.8-13.5 15.8-26.3 24.1-38.2 14.9-1.3 30-2 45.2-2 15.1 0 30.2.7 45 1.9 8.3 11.9 16.4 24.6 24.2 38 7.6 13.1 14.5 26.4 20.8 39.8-6.3 13.4-13.2 26.8-20.7 39.9zm32.3-13c5.4 13.4 10 26.8 13.8 39.8-13.1 3.2-26.9 5.9-41.2 8 4.9-7.7 9.8-15.6 14.4-23.7 4.6-8 8.9-16.1 13-24.1zM421.2 430c-9.3-9.6-18.6-20.3-27.8-32 9 .4 18.2.7 27.5.7 9.4 0 18.7-.2 27.8-.7-9 11.7-18.3 22.4-27.5 32zm-74.4-58.9c-14.2-2.1-27.9-4.7-41-7.9 3.7-12.9 8.3-26.2 13.5-39.5 4.1 8 8.4 16 13.1 24 4.7 8 9.5 15.8 14.4 23.4zM420.7 163c9.3 9.6 18.6 20.3 27.8 32-9-.4-18.2-.7-27.5-.7-9.4 0-18.7.2-27.8.7 9-11.7 18.3-22.4 27.5-32zm-74 58.9c-4.9 7.7-9.8 15.6-14.4 23.7-4.6 8-8.9 16-13 24-5.4-13.4-10-26.8-13.8-39.8 13.1-3.1 26.9-5.8 41.2-7.9zm-90.5 125.2c-35.4-15.1-58.3-34.9-58.3-50.6 0-15.7 22.9-35.6 58.3-50.6 8.6-3.7 18-7 27.7-10.1 5.7 19.6 13.2 40 22.5 60.9-9.2 20.8-16.6 41.1-22.2 60.6-9.9-3.1-19.3-6.5-28-10.2zM310 490c-13.6-7.8-19.5-37.5-14.9-75.7 1.1-9.4 2.9-19.3 5.1-29.4 19.6 4.8 41 8.5 63.5 10.9 13.5 18.5 27.5 35.3 41.6 50-32.6 30.3-63.2 46.9-84 46.9-4.5-.1-8.3-1-11.3-2.7zm237.2-76.2c4.7 38.2-1.1 67.9-14.6 75.8-3 1.8-6.9 2.6-11.5 2.6-20.7 0-51.4-16.5-84-46.6 14-14.7 28-31.4 41.3-49.9 22.6-2.4 44-6.1 63.6-11 2.3 10.1 4.1 19.8 5.2 29.1zm38.5-66.7c-8.6 3.7-18 7-27.7 10.1-5.7-19.6-13.2-40-22.5-60.9 9.2-20.8 16.6-41.1 22.2-60.6 9.9 3.1 19.3 6.5 28.1 10.2 35.4 15.1 58.3 34.9 58.3 50.6-.1 15.7-23 35.6-58.4 50.6zM320.8 78.4z\\"/>
-        <circle cx=\\"420.9\\" cy=\\"296.5\\" r=\\"45.7\\"/>
-        <path d=\\"M520.5 78.1z\\"/>
-    </g>
-</svg>
-",
-  "src/reportWebVitals.ts": "import { ReportHandler } from 'web-vitals';
-
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(
-      ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-        getCLS(onPerfEntry);
-        getFID(onPerfEntry);
-        getFCP(onPerfEntry);
-        getLCP(onPerfEntry);
-        getTTFB(onPerfEntry);
-      },
-      () => {}
-    );
-  }
-};
-
-export default reportWebVitals;",
-  "src/setupTests.ts": "// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-",
+  "src/react-app-env.d.ts": "/// <reference types=\\"react-scripts\\" />",
   "tsconfig.dev.json": Object {
     "//": "~~ Generated by projen. To modify, edit .projenrc.js and run \\"npx projen\\".",
     "compilerOptions": Object {

commit 334f742e8fe3b6a6bf3d5ae8608e356fb0f1b454

fix(react): ReactTypeScript project not synthing sample code (#1918)
This is caused by: https://github.com/projen/projen/commit/41c95b73c06ef71bfdd2d146ad9f6263bdbb6db8.

The issue is the reactTypeDef is being synthed before the sampleDir and as such the sample code tries to synth in a non-empty dir which skips this.

This PR simplifies the process by just treating reactTypeDef as just a regular sample file and as such will be synthed along with the other files. This PR does however cause a potentially breaking change as the reactTypeDef has been removed as a public property. 
 
Tested locally and can confirm all files synth correctly and the project now builds with no errors.

Fixes: #1915

BREAKING CHANGE: `reactTypeDef` is no longer exposed on React project types, and `ReactTypeDef` and `ReactTypeDefOptions` are deprecated. This file is now created as a sample file so it will be managed by the user (and they can edit it, delete it, etc.) instead of by projen. Create a `SourceCode` file instead if you would like to use a managed react type definitions file in your project.

---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

src/web/react.ts
@@ -114,13 +114,6 @@ export class ReactTypeScriptProject extends TypeScriptAppProject {
    */
   public readonly srcdir: string;
 
-  /**
-   * TypeScript definition file included that ensures React types are picked
-   * up by the TypeScript compiler.
-   *
-   */
-  public readonly reactTypeDef: ReactTypeDef;
-
   constructor(options: ReactTypeScriptProjectOptions) {
     const defaultOptions = {
       srcdir: "src",
@@ -161,11 +154,6 @@ export class ReactTypeScriptProject extends TypeScriptAppProject {
     this.srcdir = options.srcdir ?? "src";
 
     new ReactComponent(this, { typescript: true, rewire: options.rewire });
-
-    this.reactTypeDef = new ReactTypeDef(
-      this,
-      `${this.srcdir}/react-app-env.d.ts`
-    );
 
     // generate sample code in `src` and `public` if these directories are empty or non-existent.
     if (options.sampleCode ?? true) {
@@ -456,11 +444,14 @@ class ReactSampleCode extends Component {
       "",
     ];
 
+    const reactTypeDef = ['/// <reference types="react-scripts" />'];
+
     // js/ts not jsx/tsx
     const fileExtWithoutX = this.fileExt.replace("x", "");
 
     new SampleDir(project, this.srcdir, {
       files: {
+        "react-app-env.d.ts": reactTypeDef.join("\n"),
         "logo.svg": logoSvg.join("\n"),
         ["App." + this.fileExt]: appJsx.join("\n"),
         ["App.test." + this.fileExt]: appTestJsx.join("\n"),
@@ -474,8 +465,14 @@ class ReactSampleCode extends Component {
   }
 }
 
+/**
+ * @deprecated No longer used.
+ */
 export interface ReactTypeDefOptions extends FileBaseOptions {}
 
+/**
+ * @deprecated No longer used.
+ */
 export class ReactTypeDef extends FileBase {
   constructor(
     project: ReactTypeScriptProject,

test/web/__snapshots__/react-project.test.ts.snap
@@ -1059,6 +1059,7 @@ reportWebVitals();
     </g>
 </svg>
 ",
+  "src/react-app-env.d.ts": "/// <reference types=\\"react-scripts\\" />",
   "src/reportWebVitals.js": "import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {

test/web/__snapshots__/react-ts-project.test.ts.snap
@@ -18,7 +18,6 @@ Object {
 /.projen/tasks.json linguist-generated
 /LICENSE linguist-generated
 /package.json linguist-generated
-/src/react-app-env.d.ts linguist-generated
 /tsconfig.dev.json linguist-generated
 /tsconfig.json linguist-generated
 /yarn.lock linguist-generated",
@@ -254,7 +253,6 @@ jspm_packages/
 /lib
 /dist/
 /build/
-!/src/react-app-env.d.ts
 ",
   ".mergify.yml": "# ~~ Generated by projen. To modify, edit .projenrc.js and run \\"npx projen\\".
 
@@ -378,7 +376,6 @@ tsconfig.tsbuildinfo
       ".projen/files.json",
       ".projen/tasks.json",
       "LICENSE",
-      "src/react-app-env.d.ts",
       "tsconfig.dev.json",
       "tsconfig.json",
     ],
@@ -861,7 +858,144 @@ tsconfig.tsbuildinfo
 User-agent: *
 Disallow:
 ",
+  "src/App.css": ".App {
+  text-align: center;
+}
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+",
+  "src/App.test.tsx": "import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+",
+  "src/App.tsx": "import logo from './logo.svg';
+import './App.css'
+
+function App() {
+  return (
+   <div className=\\"App\\">
+      <header className=\\"App-header\\">
+        <img src={logo} className=\\"App-logo\\" alt=\\"logo\\" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className=\\"App-link\\"
+          href=\\"https://reactjs.org\\"
+          target=\\"_blank\\"
+          rel=\\"noopener noreferrer\\"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+",
+  "src/index.css": "body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+",
+  "src/index.tsx": "import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+",
+  "src/logo.svg": "<svg xmlns=\\"http://www.w3.org/2000/svg\\" viewBox=\\"0 0 841.9 595.3\\">
+    <g fill=\\"#61DAFB\\">
+        <path d=\\"M666.3 296.5c0-32.5-40.7-63.3-103.1-82.4 14.4-63.6 8-114.2-20.2-130.4-6.5-3.8-14.1-5.6-22.4-5.6v22.3c4.6 0 8.3.9 11.4 2.6 13.6 7.8 19.5 37.5 14.9 75.7-1.1 9.4-2.9 19.3-5.1 29.4-19.6-4.8-41-8.5-63.5-10.9-13.5-18.5-27.5-35.3-41.6-50 32.6-30.3 63.2-46.9 84-46.9V78c-27.5 0-63.5 19.6-99.9 53.6-36.4-33.8-72.4-53.2-99.9-53.2v22.3c20.7 0 51.4 16.5 84 46.6-14 14.7-28 31.4-41.3 49.9-22.6 2.4-44 6.1-63.6 11-2.3-10-4-19.7-5.2-29-4.7-38.2 1.1-67.9 14.6-75.8 3-1.8 6.9-2.6 11.5-2.6V78.5c-8.4 0-16 1.8-22.6 5.6-28.1 16.2-34.4 66.7-19.9 130.1-62.2 19.2-102.7 49.9-102.7 82.3 0 32.5 40.7 63.3 103.1 82.4-14.4 63.6-8 114.2 20.2 130.4 6.5 3.8 14.1 5.6 22.5 5.6 27.5 0 63.5-19.6 99.9-53.6 36.4 33.8 72.4 53.2 99.9 53.2 8.4 0 16-1.8 22.6-5.6 28.1-16.2 34.4-66.7 19.9-130.1 62-19.1 102.5-49.9 102.5-82.3zm-130.2-66.7c-3.7 12.9-8.3 26.2-13.5 39.5-4.1-8-8.4-16-13.1-24-4.6-8-9.5-15.8-14.4-23.4 14.2 2.1 27.9 4.7 41 7.9zm-45.8 106.5c-7.8 13.5-15.8 26.3-24.1 38.2-14.9 1.3-30 2-45.2 2-15.1 0-30.2-.7-45-1.9-8.3-11.9-16.4-24.6-24.2-38-7.6-13.1-14.5-26.4-20.8-39.8 6.2-13.4 13.2-26.8 20.7-39.9 7.8-13.5 15.8-26.3 24.1-38.2 14.9-1.3 30-2 45.2-2 15.1 0 30.2.7 45 1.9 8.3 11.9 16.4 24.6 24.2 38 7.6 13.1 14.5 26.4 20.8 39.8-6.3 13.4-13.2 26.8-20.7 39.9zm32.3-13c5.4 13.4 10 26.8 13.8 39.8-13.1 3.2-26.9 5.9-41.2 8 4.9-7.7 9.8-15.6 14.4-23.7 4.6-8 8.9-16.1 13-24.1zM421.2 430c-9.3-9.6-18.6-20.3-27.8-32 9 .4 18.2.7 27.5.7 9.4 0 18.7-.2 27.8-.7-9 11.7-18.3 22.4-27.5 32zm-74.4-58.9c-14.2-2.1-27.9-4.7-41-7.9 3.7-12.9 8.3-26.2 13.5-39.5 4.1 8 8.4 16 13.1 24 4.7 8 9.5 15.8 14.4 23.4zM420.7 163c9.3 9.6 18.6 20.3 27.8 32-9-.4-18.2-.7-27.5-.7-9.4 0-18.7.2-27.8.7 9-11.7 18.3-22.4 27.5-32zm-74 58.9c-4.9 7.7-9.8 15.6-14.4 23.7-4.6 8-8.9 16-13 24-5.4-13.4-10-26.8-13.8-39.8 13.1-3.1 26.9-5.8 41.2-7.9zm-90.5 125.2c-35.4-15.1-58.3-34.9-58.3-50.6 0-15.7 22.9-35.6 58.3-50.6 8.6-3.7 18-7 27.7-10.1 5.7 19.6 13.2 40 22.5 60.9-9.2 20.8-16.6 41.1-22.2 60.6-9.9-3.1-19.3-6.5-28-10.2zM310 490c-13.6-7.8-19.5-37.5-14.9-75.7 1.1-9.4 2.9-19.3 5.1-29.4 19.6 4.8 41 8.5 63.5 10.9 13.5 18.5 27.5 35.3 41.6 50-32.6 30.3-63.2 46.9-84 46.9-4.5-.1-8.3-1-11.3-2.7zm237.2-76.2c4.7 38.2-1.1 67.9-14.6 75.8-3 1.8-6.9 2.6-11.5 2.6-20.7 0-51.4-16.5-84-46.6 14-14.7 28-31.4 41.3-49.9 22.6-2.4 44-6.1 63.6-11 2.3 10.1 4.1 19.8 5.2 29.1zm38.5-66.7c-8.6 3.7-18 7-27.7 10.1-5.7-19.6-13.2-40-22.5-60.9 9.2-20.8 16.6-41.1 22.2-60.6 9.9 3.1 19.3 6.5 28.1 10.2 35.4 15.1 58.3 34.9 58.3 50.6-.1 15.7-23 35.6-58.4 50.6zM320.8 78.4z\\"/>
+        <circle cx=\\"420.9\\" cy=\\"296.5\\" r=\\"45.7\\"/>
+        <path d=\\"M520.5 78.1z\\"/>
+    </g>
+</svg>
+",
   "src/react-app-env.d.ts": "/// <reference types=\\"react-scripts\\" />",
+  "src/reportWebVitals.ts": "import { ReportHandler } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(
+      ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      },
+      () => {}
+    );
+  }
+};
+
+export default reportWebVitals;",
+  "src/setupTests.ts": "// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+",
   "tsconfig.dev.json": Object {
     "//": "~~ Generated by projen. To modify, edit .projenrc.js and run \\"npx projen\\".",
     "compilerOptions": Object {