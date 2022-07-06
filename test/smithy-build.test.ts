176e64bcb67977437a8ec102e3891b54bdd0a9b5
Mayur Mahrotri <mayur@stedi.com>
2022-07-05 20:20:30 -0400

    chore: legible tests

--- test/smithy-build.test.ts
+++ test/smithy-build.test.ts
@@ -2,44 +2,51 @@ import { SmithyBuild } from "../src/smithy-build";
 import { synthSnapshot, TestProject } from "./util";
 
 test("adds default smithy-build.json file", () => {
+  // WHEN
   const project = new TestProject();
-
   new SmithyBuild(project, {});
-
   const snps = synthSnapshot(project);
+
+  // THEN
   expect(snps["smithy-build.json"].version).toBe("1.0");
 });
 
 test("does not add smithy-build.json file by default", () => {
+  // WHEN
   const project = new TestProject();
-
   const snps = synthSnapshot(project);
+
+  // THEN
   expect(snps["smithy-build.json"]).toBeUndefined();
 });
 
 test("can add imports", () => {
+  // GIVEN
   const project = new TestProject();
-
   const smithyBuild = new SmithyBuild(project, {});
-  smithyBuild.addImport("foo.json");
 
+  // WHEN
+  smithyBuild.addImport("foo.json");
   const snps = synthSnapshot(project);
 
+  // THEN
   expect(snps["smithy-build.json"].imports).toContain("foo.json");
 });
 
 test("can add plugins", () => {
+  // GIVEN
   const project = new TestProject();
-
   const smithyBuild = new SmithyBuild(project, {});
+
+  // WHEN
   smithyBuild.addPlugins({
     "my-plugin": {
       service: "my-service",
     },
   });
-
   const snps = synthSnapshot(project);
 
+  // THEN
   expect(snps["smithy-build.json"].plugins).toStrictEqual({
     "my-plugin": {
       service: "my-service",
@@ -48,9 +55,11 @@ test("can add plugins", () => {
 });
 
 test("can add projections", () => {
+  // GIVEN
   const project = new TestProject();
-
   const smithyBuild = new SmithyBuild(project, {});
+
+  // WHEN
   smithyBuild.addProjections({
     testProjection: {
       imports: ["foo.json"],
@@ -62,9 +71,9 @@ test("can add projections", () => {
       ],
     },
   });
-
   const snps = synthSnapshot(project);
 
+  // THEN
   expect(snps["smithy-build.json"].projections).toStrictEqual({
     testProjection: {
       imports: ["foo.json"],
