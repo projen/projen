e1a1a164b7f6fd5bd4f10e38744618efbef1c117
Amir Szekely <kichik@gmail.com>
2022-07-30 14:55:52 -0700

    eslint

--- src/task-runtime.ts
+++ src/task-runtime.ts
@@ -150,7 +150,10 @@ class RunTask {
         let command = "";
         let hasError = false;
         const cmd = exec.split(" ")[0];
-        if (platform() == "win32" && ["mkdir", "mv", "rm", "cp", "cd"].includes(cmd)) {
+        if (
+          platform() == "win32" &&
+          ["mkdir", "mv", "rm", "cp", "cd"].includes(cmd)
+        ) {
           command = `shx ${exec}`;
         } else {
           command = exec;
