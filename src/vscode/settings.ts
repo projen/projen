c0191cebc593b4372091191fcfa9c59cea1a6307
Momo Kornher <kornherm@amazon.co.uk>
2022-08-19 15:19:00 +0100

    feat(vscode): support VS Code workspace settings
    Settings keys languages are not further typed as there are A LOT of
    them and plugins can add any new values to it.
    
    Setting values are not typed as they are essentially of type JSON which is
    difficult to express in TS and a jsii compatible way.

---
+++ src/vscode/settings.ts
@@ -0,0 +1,60 @@
+import { Component } from "../component";
+import { JsonFile } from "../json";
+import { VsCode } from "./vscode";
+
+/**
+ * VS Code Workspace settings
+ * Source: https://code.visualstudio.com/docs/getstarted/settings#_workspace-settings
+ */
+export class VsCodeSettings extends Component {
+  private readonly content: any;
+
+  constructor(vscode: VsCode) {
+    super(vscode.project);
+
+    this.content = {};
+
+    new JsonFile(vscode.project, ".vscode/settings.json", {
+      omitEmpty: false,
+      obj: this.content,
+    });
+  }
+
+  /**
+   * Adds a workspace setting
+   *
+   * @param setting The setting ID
+   * @param value The value of the setting
+   * @param language Scope the setting to a specific language
+   */
+  public addSetting(setting: string, value: unknown, language?: string) {
+    if (language) {
+      this.content[`[${language}]`] = this.content[`[${language}]`] ?? {};
+      this.content[`[${language}]`][setting] = value;
+    } else {
+      this.content[setting] = value;
+    }
+  }
+
+  /**
+   * Adds a workspace setting
+   *
+   * @param settings Array structure: [setting: string, value: any, languages?: string[]]
+   */
+  public addSettings(
+    settings: Record<string, unknown>,
+    languages?: string | string[]
+  ) {
+    if (Array.isArray(languages)) {
+      languages.forEach((language) => {
+        Object.entries(settings).forEach(([setting, value]) =>
+          this.addSetting(setting, value, language)
+        );
+      });
+    } else {
+      Object.entries(settings).forEach(([setting, value]) =>
+        this.addSetting(setting, value, languages)
+      );
+    }
+  }
+}
--- src/vscode/vscode.ts
+++ src/vscode/vscode.ts
@@ -1,9 +1,11 @@
 import { Component } from "../component";
 import { Project } from "../project";
 import { VsCodeLaunchConfig } from "./launch-config";
+import { VsCodeSettings } from "./settings";
 
 export class VsCode extends Component {
   private _launchConfig?: VsCodeLaunchConfig;
+  private _settings?: VsCodeSettings;
 
   constructor(project: Project) {
     super(project);
@@ -15,5 +17,13 @@ export class VsCode extends Component {
     }
 
     return this._launchConfig;
+  }
+
+  public get settings() {
+    if (!this._settings) {
+      this._settings = new VsCodeSettings(this);
+    }
+
+    return this._settings;
   }
 }
