projen/projen

commit 46a41675609a5145a79d5eadac18907ec4c96629

feat(tasks): allow passing arguments to tasks (#2246)
Closes: #356

Basic implementation of allowing arguments to be passed through to tasks. Currently not used by any project tasks, but this can be added in future.

Could be extended in future to support well defined (positional) args and options.

---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

src/cli/index.ts
@@ -14,6 +14,7 @@ async function main() {
   ya.commandDir("cmds");
 
   const runtime = new TaskRuntime(".");
+  ya.parserConfiguration({ "unknown-options-as-args": true });
   discoverTaskCommands(runtime, ya);
 
   ya.recommendCommands();

src/cli/tasks.ts
@@ -1,6 +1,7 @@
 import * as chalk from "chalk";
 import * as yargs from "yargs";
 import * as logging from "../logging";
+import { TaskSpec } from "../task-model";
 import { TaskRuntime } from "../task-runtime";
 
 /**
@@ -10,15 +11,22 @@ import { TaskRuntime } from "../task-runtime";
 export function discoverTaskCommands(runtime: TaskRuntime, ya: yargs.Argv) {
   const tasks = runtime.manifest.tasks ?? {};
   for (const task of Object.values(tasks)) {
-    ya.command(
-      task.name,
-      task.description ?? "",
-      taskCommandHandler(task.name)
-    );
+    ya.command(task.name, task.description ?? "", taskCommandHandler(task));
   }
 
-  function taskCommandHandler(taskName: string) {
+  function taskCommandHandler(task: TaskSpec) {
     return (args: yargs.Argv) => {
+      const taskReceivesArgs = task.steps?.reduce(
+        (receiveArgs, step) => receiveArgs || (step.receiveArgs ?? false),
+        false
+      );
+      let taskArgs: Array<string | number> = [];
+      if (taskReceivesArgs) {
+        args.strict(false);
+        args.strictCommands(false);
+        taskArgs = args.argv._.slice(1);
+      }
+
       args.option("inspect", {
         alias: "i",
         desc: "show all steps in this task",
@@ -27,10 +35,10 @@ export function discoverTaskCommands(runtime: TaskRuntime, ya: yargs.Argv) {
       const argv = args.argv;
 
       if (argv.inspect) {
-        return inspectTask(taskName);
+        return inspectTask(task.name);
       } else {
         try {
-          runtime.runTask(taskName);
+          runtime.runTask(task.name, [], taskArgs);
         } catch (e) {
           logging.error(e.message);
           process.exit(1);

src/task-model.ts
@@ -81,6 +81,21 @@ export interface TaskStepOptions {
    * @default - determined by the task
    */
   readonly cwd?: string;
+
+  /**
+   * Should this step receive args passed to the task.
+   *
+   * If `true`, args are passed through at the end of the `exec` shell command.\
+   * The position of the args can be changed by including the marker `$@` inside the command string.
+   *
+   * If the step spawns a subtask, args are passed to the subtask.
+   * The subtask must define steps receiving args for this to have any effect.
+   *
+   * @example task.exec("echo Hello $@ World!", { receiveArgs: true });
+   *
+   * @default false
+   */
+  readonly receiveArgs?: boolean;
 }
 
 /**

src/task-runtime.ts
@@ -10,6 +10,7 @@ import * as logging from "./logging";
 import { TasksManifest, TaskSpec } from "./task-model";
 
 const ENV_TRIM_LEN = 20;
+const ARGS_MARKER = "$@";
 
 /**
  * The runtime component of the tasks engine.
@@ -62,13 +63,17 @@ export class TaskRuntime {
    * Runs the task.
    * @param name The task name.
    */
-  public runTask(name: string, parents: string[] = []) {
+  public runTask(
+    name: string,
+    parents: string[] = [],
+    args: Array<string | number> = []
+  ) {
     const task = this.tryFindTask(name);
     if (!task) {
       throw new Error(`cannot find command ${task}`);
     }
 
-    new RunTask(this, task, parents);
+    new RunTask(this, task, parents, args);
   }
 }
 
@@ -81,7 +86,8 @@ class RunTask {
   constructor(
     private readonly runtime: TaskRuntime,
     private readonly task: TaskSpec,
-    parents: string[] = []
+    parents: string[] = [],
+    args: Array<string | number> = []
   ) {
     this.workdir = task.cwd ?? this.runtime.workdir;
 
@@ -137,7 +143,11 @@ class RunTask {
       }
 
       if (step.spawn) {
-        this.runtime.runTask(step.spawn, [...this.parents, this.task.name]);
+        this.runtime.runTask(
+          step.spawn,
+          [...this.parents, this.task.name],
+          step.receiveArgs ? args : []
+        );
       }
 
       const execs = step.exec ? [step.exec] : [];
@@ -158,6 +168,15 @@ class RunTask {
         } else {
           command = exec;
         }
+
+        if (step.receiveArgs) {
+          if (command.includes(ARGS_MARKER)) {
+            command = command.replace(ARGS_MARKER, args.join(" "));
+          } else {
+            command = [command, ...args].join(" ");
+          }
+        }
+
         const cwd = step.cwd;
         try {
           const result = this.shell({

src/task.ts
@@ -16,6 +16,14 @@ export interface TaskOptions extends TaskCommonOptions {
    * List of task steps to run.
    */
   readonly steps?: TaskStep[];
+
+  /**
+   * Should the provided `exec` shell command receive args passed to the task.
+   * @see {@link TaskStepOptions.receiveArgs}
+   *
+   * @default false
+   */
+  readonly receiveArgs?: boolean;
 }
 
 /**
@@ -57,7 +65,7 @@ export class Task {
     }
 
     if (props.exec) {
-      this.exec(props.exec);
+      this.exec(props.exec, { receiveArgs: props.receiveArgs });
     }
   }
 

test/tasks/runtime.test.ts
@@ -232,10 +232,57 @@ test("requiredEnv can be used to specify required environment variables", () => 
   ).toStrictEqual(["env1 env2 env3"]);
 });
 
+test("exec can receive args", () => {
+  // GIVEN
+  const p = new TestProject();
+
+  // WHEN
+  p.addTask("test1", {
+    exec: "echo hello",
+    receiveArgs: true,
+  });
+
+  // THEN
+  expect(
+    executeTask(p, "test1", {}, ["world", "and", "other", "planets"])
+  ).toEqual(["hello world and other planets"]);
+});
+
+test("exec can receive args at marker", () => {
+  // GIVEN
+  const p = new TestProject();
+
+  // WHEN
+  p.addTask("test1", {
+    exec: "echo hello $@ world",
+    receiveArgs: true,
+  });
+
+  // THEN
+  expect(executeTask(p, "test1", {}, ["beautiful", "and", "round"])).toEqual([
+    "hello beautiful and round world",
+  ]);
+});
+
+test("spawn can receive args", () => {
+  const p = new TestProject();
+  const parent = p.addTask("parent");
+  const child = p.addTask("child", {
+    exec: 'echo "child: [$@]"',
+    receiveArgs: true,
+  });
+  parent.spawn(child, { receiveArgs: true });
+
+  expect(executeTask(p, "parent", {}, ["one", "--two", "-3"])).toStrictEqual([
+    "child: [one --two -3]",
+  ]);
+});
+
 function executeTask(
   p: Project,
   taskName: string,
-  env: Record<string, string> = {}
+  env: Record<string, string> = {},
+  additionalArgs: string[] = []
 ) {
   p.synth();
 
@@ -243,12 +290,16 @@ function executeTask(
     (x) => `"${x}"`
   );
 
-  const result = spawnSync(`"${process.execPath}"`, args, {
-    cwd: p.outdir,
-    shell: true,
-    env: { ...process.env, ...env },
-    timeout: 10_000, // let's try to catch hanging processes sooner than later
-  });
+  const result = spawnSync(
+    `"${process.execPath}"`,
+    [...args, ...additionalArgs],
+    {
+      cwd: p.outdir,
+      shell: true,
+      env: { ...process.env, ...env },
+      timeout: 10_000, // let's try to catch hanging processes sooner than later
+    }
+  );
   if (result.status !== 0) {
     throw new Error(`non-zero exit code: ${result.stderr.toString("utf-8")}`);
   }

test/tasks/tasks.test.ts
@@ -355,6 +355,37 @@ test("it is possible to edit the description", () => {
   expect(files[".projen/tasks.json"].tasks.t2.description).toBe("world");
 });
 
+test("steps can receive args", () => {
+  const p = new TestProject();
+
+  // WHEN
+  const hello = p.addTask("hello");
+  const world = p.addTask("world", {
+    exec: "echo $@ world",
+    receiveArgs: true,
+  });
+
+  hello.exec("echo hello", { receiveArgs: true });
+  hello.spawn(world, { receiveArgs: true });
+
+  // THEN
+  expectManifest(p, {
+    tasks: {
+      hello: {
+        name: "hello",
+        steps: [
+          { exec: "echo hello", receiveArgs: true },
+          { spawn: "world", receiveArgs: true },
+        ],
+      },
+      world: {
+        name: "world",
+        steps: [{ exec: "echo $@ world", receiveArgs: true }],
+      },
+    },
+  });
+});
+
 function expectManifest(p: Project, toStrictEqual: TasksManifest) {
   const manifest = synthTasksManifest(p);
   delete manifest["//"];

commit 9f2c7e6f70e77c436032f3549f597fbea52f06b3

fix(tasks): `projen new` throws "unknown command" error for external projects (#2252)
Fixes #2250

This fixes a regression introduced in #2246

---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

src/cli/index.ts
@@ -14,7 +14,6 @@ async function main() {
   ya.commandDir("cmds");
 
   const runtime = new TaskRuntime(".");
-  ya.parserConfiguration({ "unknown-options-as-args": true });
   discoverTaskCommands(runtime, ya);
 
   ya.recommendCommands();

src/cli/tasks.ts
@@ -1,5 +1,6 @@
 import * as chalk from "chalk";
 import * as yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import * as logging from "../logging";
 import { TaskSpec } from "../task-model";
 import { TaskRuntime } from "../task-runtime";
@@ -24,7 +25,7 @@ export function discoverTaskCommands(runtime: TaskRuntime, ya: yargs.Argv) {
       if (taskReceivesArgs) {
         args.strict(false);
         args.strictCommands(false);
-        taskArgs = args.argv._.slice(1);
+        taskArgs = hideBin(process.argv).slice(1);
       }
 
       args.option("inspect", {

test/__snapshots__/new.test.ts.snap
@@ -371,6 +371,20 @@ project.synth();",
 }
 `;
 
+exports[`projen new with unknown option works 1`] = `
+"const { javascript } = require(\\"projen\\");
+const project = new javascript.NodeProject({
+  defaultReleaseBranch: \\"main\\",
+  name: \\"my-project\\",
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The \\"name\\" in package.json. */
+});
+project.synth();"
+`;
+
 exports[`projenrc-json creates java project 1`] = `
 Object {
   ".gitattributes": "# ~~ Generated by projen. To modify, edit .projenrc.js and run \\"npx projen\\".

test/new.test.ts
@@ -173,6 +173,21 @@ test("projen new --no-comments", () => {
   });
 });
 
+test("projen new with unknown option works", () => {
+  withProjectDir((projectdir) => {
+    execProjenCLI(projectdir, [
+      "new",
+      "node",
+      "--DOES_NOT_EXIST",
+      "--no-synth",
+    ]);
+
+    const projenrc = directorySnapshot(projectdir)[".projenrc.js"];
+    expect(projenrc).toBeDefined();
+    expect(projenrc).toMatchSnapshot();
+  });
+});
+
 test("creating node project with enum-typed CLI arg", () => {
   withProjectDir((projectdir) => {
     execProjenCLI(projectdir, [