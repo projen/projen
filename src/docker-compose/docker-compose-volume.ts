ec4c6b16c5a52981fbefa6e07d5cc7b6b1499c26
Kenneth Wußmann <kenneth@wussmann.net>
2023-01-31 12:06:21 +0100

    chore(docker-compose): split docker-compose and move to own submodule (#2403)
    As proposed by me in #2397, I'd like to implement networks into the `DockerCompose` component, but as the file was already large I wanted to take the opportunity to refactor it beforehand.
    
    Therefore, I:
    1. Kept all the logic and code the same, but moved the docker-compose component to an own directory
    2. Split the larger file into multiple smaller ones, by resource type and purpose, to make it easier to navigate
    
    I don't expect this to be a breaking change. All interfaces are still exported to the same path and with the same name. I outlined that in the above mentioned issue.
    
    ---
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

---
+++ src/docker-compose/docker-compose-volume.ts
@@ -0,0 +1,75 @@
+/**
+ * Volume configuration
+ */
+export interface DockerComposeVolumeConfig {
+  /**
+   * Driver to use for the volume
+   * @default - value is not provided
+   */
+  readonly driver?: string;
+
+  /**
+   * Options to provide to the driver.
+   */
+  readonly driverOpts?: Record<string, string>;
+
+  /**
+   * Set to true to indicate that the volume is externally created.
+   * @default - unset, indicating that docker-compose creates the volume
+   */
+  readonly external?: boolean;
+
+  /**
+   * Name of the volume for when the volume name isn't going to work in YAML.
+   * @default - unset, indicating that docker-compose creates volumes as usual
+   */
+  readonly name?: string;
+}
+
+/**
+ * Volume binding information.
+ */
+export interface IDockerComposeVolumeBinding {
+  /**
+   * Binds the requested volume to the docker-compose volume configuration and
+   * provide mounting instructions for synthesis.
+   * @param volumeConfig the volume configuration
+   * @returns mounting instructions for the service.
+   */
+  bind(volumeConfig: IDockerComposeVolumeConfig): DockerComposeVolumeMount;
+}
+
+/**
+ * Storage for volume configuration.
+ */
+export interface IDockerComposeVolumeConfig {
+  /**
+   * Add volume configuration to the repository.
+   * @param volumeName
+   * @param configuration
+   */
+  addVolumeConfiguration(
+    volumeName: string,
+    configuration: DockerComposeVolumeConfig
+  ): void;
+}
+
+/**
+ * Service volume mounting information.
+ */
+export interface DockerComposeVolumeMount {
+  /**
+   * Type of volume.
+   */
+  readonly type: string;
+
+  /**
+   * Volume source
+   */
+  readonly source: string;
+
+  /**
+   * Volume target
+   */
+  readonly target: string;
+}
