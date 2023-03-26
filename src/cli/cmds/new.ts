a686cbc533c6de0b67fcde791a1083a0d447b89c
Momo Kornher <kornherm@amazon.co.uk>
2023-03-26 21:11:44 +0100

    fix(cli): command suggestion prevents use of external projects

--- src/cli/cmds/new.ts
+++ src/cli/cmds/new.ts
@@ -108,30 +108,45 @@ class Command implements yargs.CommandModule {
       });
     }
 
+    args.strictCommands(false);
+    args
+      .command({
+        command: "*",
+        describe: false,
+        handler,
+      })
+      .middleware((argv) => {
+        argv.projectTypeName = argv._[1];
+      }, true);
+
     return args;
   }
 
   public async handler(args: any) {
-    // handle --from which means we want to first install a jsii module and then
-    // create a project defined within this module.
-    if (args.from) {
-      return initProjectFromModule(process.cwd(), args.from, args);
-    }
-
-    // project type is defined but was not matched by yargs, so print the list of supported types
-    if (args.projectTypeName) {
-      console.log(
-        `Invalid project type ${args.projectTypeName}. Supported types:`
-      );
-      for (const pjid of inventory.discover().map((x) => x.pjid)) {
-        console.log(`  ${pjid}`);
-      }
-      return;
-    }
-
-    // Handles the use case that nothing was specified since PROJECT-TYPE is now an optional positional parameter
-    yargs.showHelp();
+    return handler(args);
   }
+}
+
+async function handler(args: any) {
+  // handle --from which means we want to first install a jsii module and then
+  // create a project defined within this module.
+  if (args.from) {
+    return initProjectFromModule(process.cwd(), args.from, args);
+  }
+
+  // project type is defined but was not matched by yargs, so print the list of supported types
+  if (args.projectTypeName) {
+    console.log(
+      `Invalid project type ${args.projectTypeName}. Supported types:`
+    );
+    for (const pjid of inventory.discover().map((x) => x.pjid)) {
+      console.log(`  ${pjid}`);
+    }
+    return;
+  }
+
+  // Handles the use case that nothing was specified since PROJECT-TYPE is now an optional positional parameter
+  yargs.showHelp();
 }
 
 /**
