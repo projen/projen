a2eabb4b18f1cbcc905a5e52dfb1e52c15cb047d
Braden Mars <bradenmars@bradenmars.me>
2023-04-13 04:24:44 -0500

    test(typescript): add unit tests for ts-config extends field.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- test/javascript/typescript-config.test.ts
+++ test/javascript/typescript-config.test.ts
@@ -1,5 +1,9 @@
 import * as ts from "typescript";
-import { NodeProject, TypescriptConfig } from "../../src/javascript";
+import {
+  NodeProject,
+  TypescriptConfig,
+  TypeScriptModuleResolution,
+} from "../../src/javascript";
 import { withProjectDir } from "../util";
 
 describe("TypescriptConfig", () => {
@@ -33,6 +37,76 @@ describe("TypescriptConfig", () => {
         "compilerOptions.outDir",
         "testOurDir"
       );
+    });
+  });
+
+  test("TypeScript should parse generated config with extensions without warnings", () => {
+    withProjectDir((outdir) => {
+      const project = new NodeProject({
+        name: "project",
+        defaultReleaseBranch: "main",
+        outdir,
+      });
+      const baseConfig = new TypescriptConfig(project, {
+        compilerOptions: { outDir: "testOurDir" },
+      });
+      const tsConfig = new TypescriptConfig(project, {
+        fileName: "tsconfig.inherit.json",
+        extends: [baseConfig],
+        compilerOptions: { allowJs: true },
+      });
+      project.synth();
+
+      const loadedBase = ts.readConfigFile(
+        baseConfig.file.absolutePath,
+        ts.sys.readFile
+      );
+      const loadedConfig = ts.readConfigFile(
+        tsConfig.file.absolutePath,
+        ts.sys.readFile
+      );
+
+      expect(loadedConfig.error).toBeUndefined();
+      expect(loadedConfig.config).toHaveProperty("extends", "tsconfig.json");
+      expect(loadedBase.config).not.toHaveProperty("extends");
+    });
+  });
+
+  test("TypeScript should parse generated config with multiple extensions", () => {
+    withProjectDir((outdir) => {
+      const project = new NodeProject({
+        name: "project",
+        defaultReleaseBranch: "main",
+        outdir,
+      });
+      new TypescriptConfig(project, {
+        fileName: "a/tsconfig.outdir.json",
+        compilerOptions: { outDir: "testOurDir" },
+      });
+      const bundlerConfig = new TypescriptConfig(project, {
+        fileName: "sub/b/d/c/tsconfig.bundler.json",
+        compilerOptions: {
+          moduleResolution: TypeScriptModuleResolution.BUNDLER,
+        },
+      });
+      const tsConfig = new TypescriptConfig(project, {
+        fileName: "sub/tsconfig.inherit.json",
+        compilerOptions: { allowJs: true },
+      });
+      tsConfig.addExtends("../a/tsconfig.outdir.json");
+      tsConfig.addExtends(bundlerConfig);
+      project.synth();
+
+      const loadedConfig = ts.readConfigFile(
+        tsConfig.file.absolutePath,
+        ts.sys.readFile
+      );
+
+      expect(loadedConfig.error).toBeUndefined();
+      expect(loadedConfig.config).toHaveProperty("extends", [
+        "../a/tsconfig.outdir.json",
+        "b/d/c/tsconfig.bundler.json",
+      ]);
     });
   });
 });
