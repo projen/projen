ac89769917edd0e4ad0302c1590670c903b2e232
Konstantin Vyatkin <tino@vtkn.io>
2023-08-14 23:06:05 +0200

    fix(file-base): marker should use projenCommand

--- test/toml.test.ts
+++ test/toml.test.ts
@@ -1,6 +1,8 @@
+import { randomUUID } from "crypto";
 import * as TOML from "@iarna/toml";
 import { synthSnapshot, TestProject } from "./util";
 import { TomlFile } from "../src";
+import { PythonProject } from "../src/python/python-project";
 
 test("toml object can be mutated before synthesis", () => {
   const prj = new TestProject();
@@ -38,4 +40,28 @@ test("toml file can contain projen marker", () => {
   const firstLine = output.split("\n")[0];
 
   expect(firstLine).toBe(`# ${file.marker}`);
+});
+
+test("marker should use correct projenrc filename and projen command", () => {
+  const projenCommand = "projen" + randomUUID();
+  const project = new PythonProject({
+    projenCommand,
+    moduleName: "hello",
+    name: "test-python-project",
+    authorName: "John Doe",
+    authorEmail: "johndoe@example.com",
+    version: "0.1.0",
+  });
+
+  new TomlFile(project, "my/toml/file-marker.toml", {
+    obj: {},
+    marker: true,
+  });
+
+  const output = synthSnapshot(project)["my/toml/file-marker.toml"];
+
+  const [firstLine] = output.split("\n", 1);
+
+  expect(firstLine).toContain(".projenrc.py");
+  expect(firstLine).toContain(projenCommand);
 });
