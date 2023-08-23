396501a6d7dda2fa43529668fd26e11b178ec7e8
Matthew Bonig <matthew.bonig@gmail.com>
2023-08-22 20:07:22 -0600

    adding tailwindcss and filling out the home page a little more

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -117,6 +117,19 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
 };
 
 module.exports = config;
