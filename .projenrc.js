projen/projen

commit 34edf99eaa7884cc279e946c56da8e322e6dc839

feat: Projects and Components are Constructs (#2974)
Resolves #1763 

Features:

- 🚧 `Project` & `Component` now extend `Construct` - making everything a construct!
- ✨ Magic AutoID feature for existing components, no hassle in the migration
- ℹ️ Basic Metadata added by default
- 📦 `Component` class now accept any construct as scope, and throw if no project can be found
- 📄 Core file types have been updated to implement thew new interface
- 🌳 An experimental `tree.json` component 

Next steps (after this PR):

- Update all components to accept any construct as scope
- Instantiate components with the current scope, instead of the project

---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

.projenrc.js
@@ -1,4 +1,4 @@
-const { cdk, javascript, JsonFile, TextFile } = require("./lib");
+const { cdk, javascript, JsonFile, ProjectTree, TextFile } = require("./lib");
 const { PROJEN_MARKER } = require("./lib/common");
 
 const project = new cdk.JsiiProject({
@@ -59,6 +59,8 @@ const project = new cdk.JsiiProject({
     "esbuild",
     "all-contributors-cli",
   ],
+
+  peerDeps: ["constructs@^10.0.0"],
 
   depsUpgradeOptions: {
     // markmac depends on projen, we are excluding it here to avoid a circular update loop
@@ -267,5 +269,7 @@ setupBundleTaskRunner();
 // fixes feedback loop where projen contibutors run "build"
 // but not all files are updated
 project.postCompileTask.spawn(project.defaultTask);
+
+new ProjectTree(project);
 
 project.synth();

.gitattributes
@@ -21,6 +21,7 @@
 /.projen/deps.json linguist-generated
 /.projen/files.json linguist-generated
 /.projen/tasks.json linguist-generated
+/.projen/tree.json linguist-generated
 /.vscode/launch.json linguist-generated
 /docs/api/API.md linguist-generated
 /LICENSE linguist-generated

.gitignore
@@ -61,3 +61,4 @@ tsconfig.json
 !test/inventory/**
 !/.markdownlint.json
 !/.vscode/launch.json
+!/.projen/tree.json

src/component.ts
@@ -1,11 +1,39 @@
+import { Construct, IConstruct } from "constructs";
 import { Project } from "./project";
+import {
+  COMPONENT_SYMBOL,
+  isComponent,
+  findClosestProject,
+} from "./util/constructs";
+
+let currentId = 0;
+const componentId = () => {
+  return `AutoId${++currentId}`;
+};
 
 /**
  * Represents a project component.
+ * @param project
+ * @param id Unique id of the component. If not provided, an unstable AutoId is generated.
  */
-export class Component {
-  constructor(public readonly project: Project) {
-    project._addComponent(this);
+export class Component extends Construct {
+  /**
+   * Test whether the given construct is a component.
+   */
+  public static isComponent(x: any): x is Component {
+    return isComponent(x);
+  }
+
+  public readonly project: Project;
+
+  constructor(scope: IConstruct, id?: string) {
+    super(scope, id || `${new.target.name}#${componentId()}`);
+    Object.defineProperty(this, COMPONENT_SYMBOL, { value: true });
+    this.node.addMetadata("type", "component");
+    this.node.addMetadata("construct", new.target.name);
+
+    this.project = findClosestProject(scope);
+    this.project._addComponent(this);
   }
 
   /**

src/file.ts
@@ -1,11 +1,12 @@
 import { rmSync } from "fs";
 import * as path from "path";
+import { IConstruct } from "constructs";
 import { resolve } from "./_resolve";
 import { PROJEN_MARKER, PROJEN_RC } from "./common";
 import { Component } from "./component";
-import { Project } from "./project";
 import { ProjenrcFile } from "./projenrc";
 import { isExecutable, isWritable, tryReadFileSync, writeFile } from "./util";
+import { findClosestProject } from "./util/constructs";
 
 export interface FileBaseOptions {
   /**
@@ -47,7 +48,7 @@ export interface FileBaseOptions {
 
 export abstract class FileBase extends Component {
   /**
-   * The file path, relative to the project root.
+   * The file path, relative to the project's outdir.
    */
   public readonly path: string;
 
@@ -85,15 +86,31 @@ export abstract class FileBase extends Component {
   }
 
   constructor(
-    project: Project,
+    scope: IConstruct,
     filePath: string,
     options: FileBaseOptions = {}
   ) {
-    super(project);
+    const project = findClosestProject(scope);
+    const root = project.root;
+    const projectPath = path.normalize(filePath);
+    const absolutePath = path.resolve(project.outdir, projectPath);
+    const rootProjectPath = path.relative(root.outdir, absolutePath);
+    const autoId = `${new.target.name}@${projectPath}`;
+
+    // Before actually creating the file, ensure the file path is unique within the full project tree
+    // This is required because projects can create files inside their subprojects
+    if (root.tryFindFile(absolutePath) || scope.node.tryFindChild(autoId)) {
+      throw new Error(`There is already a file under ${rootProjectPath}`);
+    }
+
+    super(scope, autoId);
+    this.node.addMetadata("type", "file");
+    this.node.addMetadata("path", rootProjectPath);
 
     this.readonly = !project.ejected && (options.readonly ?? true);
     this.executable = options.executable ?? false;
-    this.path = filePath;
+    this.path = projectPath;
+    this.absolutePath = absolutePath;
     this.shouldAddMarker = options.marker ?? true;
 
     const globPattern = `/${this.path}`;
@@ -101,20 +118,6 @@ export abstract class FileBase extends Component {
     if (committed && filePath !== ".gitattributes") {
       project.annotateGenerated(`/${filePath}`);
     }
-
-    this.absolutePath = path.resolve(project.outdir, filePath);
-
-    // verify file path is unique within project tree
-    const existing = project.root.tryFindFile(this.absolutePath);
-    if (existing && existing !== this) {
-      throw new Error(
-        `There is already a file under ${path.relative(
-          project.root.outdir,
-          this.absolutePath
-        )}`
-      );
-    }
-
     const editGitignore = options.editGitignore ?? true;
     if (editGitignore) {
       this.project.addGitIgnore(`${committed ? "!" : ""}${globPattern}`);

src/gitattributes.ts
@@ -1,5 +1,5 @@
+import { IConstruct } from "constructs";
 import { IResolver, FileBase } from "./file";
-import { Project } from "./project";
 
 /**
  * Assign attributes to file names in a git repository.
@@ -9,8 +9,8 @@ import { Project } from "./project";
 export class GitAttributesFile extends FileBase {
   private readonly attributes = new Map<string, Set<string>>();
 
-  public constructor(project: Project) {
-    super(project, ".gitattributes", {
+  public constructor(scope: IConstruct) {
+    super(scope, ".gitattributes", {
       editGitignore: false,
     });
   }

src/github/workflows.ts
@@ -83,7 +83,7 @@ export class GithubWorkflow extends Component {
     name: string,
     options: GithubWorkflowOptions = {}
   ) {
-    super(github.project);
+    super(github.project, `${new.target.name}#${name}`);
 
     this.name = name;
     this.concurrency = options.concurrency;

src/gitlab/configuration.ts
@@ -56,10 +56,6 @@ export interface CiConfigurationOptions {
  */
 export class CiConfiguration extends Component {
   /**
-   * The project the configuration belongs to.
-   */
-  public readonly project: Project;
-  /**
    * The name of the configuration.
    */
   public readonly name: string;
@@ -148,7 +144,6 @@ export class CiConfiguration extends Component {
     options?: CiConfigurationOptions
   ) {
     super(project);
-    this.project = project;
     this.name = path.parse(name).name;
     this.path =
       this.name === "gitlab-ci"

commit 70520820b11c8d8136d79484573c0ea300fdfe18

fix(node): yarn fails with projen has unmet peer dependency "constructs@^10.0.0" (second attempt) (#3039)
constructs is a peer-dependency of projen. yarn (and older versions of npm) don't install peer deps automatically. 
Previously attempted to fix in https://github.com/projen/projen/pull/3037. While this was also a required change, it doesn't solve the scenario where projen is upgraded, but constructs not yet added - so projen still can't synth.

This change attempts to solve the issue by adding an explicit dependency on constructs. While not ideal, it should work. 

Issue was originally introduced in https://github.com/projen/projen/pull/2974

---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

.projenrc.js
@@ -33,6 +33,8 @@ const project = new cdk.JsiiProject({
   jsiiVersion: "5.1.x",
   typescriptVersion: "5.1.x",
 
+  deps: ["constructs@^10.0.0"],
+
   bundledDeps: [
     "conventional-changelog-config-spec",
     "yaml@^2.2.2",