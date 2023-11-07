projen/projen

commit 7a3883a4525e39e0f1c586b5210e84885a62c48b

feat: add Yarnrc component for YARN_BERRY (#3048)
## Description

This PR significantly improves `projen`'s ability to use Yarn Berry.

Relates to #2980 

## Known Limitations
- There's not a way to pass `plugins` yet. When you run `yarn plugin import <plugin name>`, it writes to `.yarnrc.yml` and stores the plugin code in `.yarn`. We'll have to figure out the best way to handle that. The good news is that Yarn v4 includes the most common plugins by default again.

---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

src/javascript/node-package.ts
@@ -12,6 +12,7 @@ import {
   minVersion,
   tryResolveDependencyVersion,
 } from "./util";
+import { Yarnrc, YarnrcOptions } from "./yarnrc";
 import { resolve as resolveJson } from "../_resolve";
 import { Component } from "../component";
 import { DependencyType } from "../dependencies";
@@ -307,6 +308,13 @@ export interface NodePackageOptions {
    * @default - fetch all scoped packages from the public npm registry
    */
   readonly scopedPackagesOptions?: ScopedPackagesOptions[];
+
+  /**
+   * Options for Yarn Berry
+   *
+   * @default - Yarn Berry v4 with all default options
+   */
+  readonly yarnBerryOptions?: YarnBerryOptions;
 }
 
 /**
@@ -577,6 +585,14 @@ export class NodePackage extends Component {
             }
           : undefined,
     };
+
+    // Configure Yarn Berry if using
+    if (
+      this.packageManager === NodePackageManager.YARN_BERRY ||
+      this.packageManager === NodePackageManager.YARN2
+    ) {
+      this.configureYarnBerry(project, options);
+    }
 
     // add tasks for scripts from options (if specified)
     // @deprecated
@@ -1485,6 +1501,57 @@ export class NodePackage extends Component {
       : this.installTask;
     runtime.runTask(taskToRun.name);
   }
+
+  private configureYarnBerry(project: Project, options: NodePackageOptions) {
+    const {
+      version = "4.0.1",
+      yarnRcOptions = {},
+      zeroInstalls = false,
+    } = options.yarnBerryOptions || {};
+    this.checkForConflictingYarnOptions(yarnRcOptions);
+
+    // Set the `packageManager` field in `package.json` to the version specified. This tells `corepack` which version
+    // of `yarn` to use.
+    this.addField("packageManager", `yarn@${version}`);
+    this.configureYarnBerryGitignore(zeroInstalls);
+
+    new Yarnrc(project, version, yarnRcOptions);
+  }
+
+  private checkForConflictingYarnOptions(yarnRcOptions: YarnrcOptions) {
+    if (this.npmAccess && yarnRcOptions.npmPublishAccess) {
+      throw new Error(
+        "Cannot set npmAccess and yarnRcOptions.npmPublishAccess at the same time."
+      );
+    }
+
+    if (this.npmRegistryUrl && yarnRcOptions.npmRegistryServer) {
+      throw new Error(
+        "Cannot set npmRegistryUrl and yarnRcOptions.npmRegistryServer at the same time."
+      );
+    }
+  }
+
+  /** See https://yarnpkg.com/getting-started/qa#which-files-should-be-gitignored */
+  private configureYarnBerryGitignore(zeroInstalls: boolean) {
+    const { gitignore } = this.project;
+
+    // These patterns are the same whether or not you're using zero-installs
+    gitignore.exclude(".yarn/*");
+    gitignore.include(
+      ".yarn/patches",
+      ".yarn/plugins",
+      ".yarn/releases",
+      ".yarn/sdks",
+      ".yarn/versions"
+    );
+
+    if (zeroInstalls) {
+      gitignore.include("!.yarn/cache");
+    } else {
+      gitignore.exclude(".pnp.*");
+    }
+  }
 }
 
 export interface PeerDependencyOptions {
@@ -1552,6 +1619,33 @@ export enum NpmAccess {
    * Package can only be accessed with credentials.
    */
   RESTRICTED = "restricted",
+}
+
+/**
+ * Configure Yarn Berry
+ */
+export interface YarnBerryOptions {
+  /**
+   * A fully specified version to use for yarn (e.g., x.x.x)
+   *
+   * @default - 4.0.1
+   */
+  readonly version?: string;
+
+  /**
+   * The yarnrc configuration.
+   *
+   * @default - a blank Yarn RC file
+   */
+  readonly yarnRcOptions?: YarnrcOptions;
+
+  /**
+   * Should zero-installs be enabled?
+   * Learn more at: https://yarnpkg.com/features/caching#zero-installs
+   *
+   * @default false
+   */
+  readonly zeroInstalls?: boolean;
 }
 
 interface NpmDependencies {

test/javascript/node-package.test.ts
@@ -4,8 +4,15 @@ import * as semver from "semver";
 import * as YAML from "yaml";
 import { Project, DependencyType, Component } from "../../src";
 import {
+  YarnCacheMigrationMode,
+  YarnChecksumBehavior,
+  YarnNodeLinker,
+  YarnNpmPublishAccess,
+} from "../../src/javascript";
+import {
   NodePackage,
   NodePackageManager,
+  NpmAccess,
 } from "../../src/javascript/node-package";
 import { minVersion } from "../../src/javascript/util";
 import { TaskRuntime } from "../../src/task-runtime";
@@ -706,4 +713,187 @@ test("project components should be able to change dependencies during preSynthes
     "test-dev-dep-2",
     "2.0.0"
   );
+});
+
+describe("yarn berry", () => {
+  test("adds the packageManager directive to package.json", () => {
+    const project = new TestProject();
+    new NodePackage(project, {
+      packageManager: NodePackageManager.YARN_BERRY,
+      yarnBerryOptions: {
+        version: "3.6.4",
+      },
+    });
+
+    const snps = synthSnapshot(project);
+
+    expect(snps["package.json"]).toHaveProperty("packageManager", "yarn@3.6.4");
+  });
+
+  test("renders .yarnrc.yml file with specified properties", () => {
+    const project = new TestProject();
+    new NodePackage(project, {
+      packageManager: NodePackageManager.YARN_BERRY,
+      yarnBerryOptions: {
+        yarnRcOptions: {
+          nodeLinker: YarnNodeLinker.NODE_MODULES,
+        },
+      },
+    });
+
+    const snps = synthSnapshot(project);
+    const yarnrcLines = snps[".yarnrc.yml"].split("\n");
+
+    expect(yarnrcLines).toContain("nodeLinker: node-modules");
+  });
+
+  describe("gitignore", () => {
+    test("produces the expected gitignore for zero-installs", () => {
+      const project = new TestProject();
+      new NodePackage(project, {
+        packageManager: NodePackageManager.YARN_BERRY,
+        yarnBerryOptions: {
+          zeroInstalls: true,
+        },
+      });
+
+      const snps = synthSnapshot(project);
+      const gitignoreLines = snps[".gitignore"].split("\n");
+      expect(gitignoreLines).toEqual(
+        expect.arrayContaining([
+          ".yarn/*",
+          "!.yarn/cache",
+          "!.yarn/patches",
+          "!.yarn/plugins",
+          "!.yarn/releases",
+          "!.yarn/sdks",
+          "!.yarn/versions",
+        ])
+      );
+    });
+
+    test("produces the expected gitignore when not using zero-installs", () => {
+      const project = new TestProject();
+      new NodePackage(project, {
+        packageManager: NodePackageManager.YARN_BERRY,
+        yarnBerryOptions: {
+          zeroInstalls: false,
+        },
+      });
+
+      const snps = synthSnapshot(project);
+      const gitignoreLines = snps[".gitignore"].split("\n");
+      expect(gitignoreLines).toEqual(
+        expect.arrayContaining([
+          ".pnp.*",
+          ".yarn/*",
+          "!.yarn/patches",
+          "!.yarn/plugins",
+          "!.yarn/releases",
+          "!.yarn/sdks",
+          "!.yarn/versions",
+        ])
+      );
+    });
+  });
+
+  describe("conflicting options", () => {
+    test("throws an error if npmRegistryUrl and npmRegistryServer are both set", () => {
+      const project = new TestProject();
+      expect(
+        () =>
+          new NodePackage(project, {
+            packageManager: NodePackageManager.YARN_BERRY,
+            npmRegistryUrl: "https://registry.npmjs.org/",
+            yarnBerryOptions: {
+              yarnRcOptions: {
+                npmRegistryServer: "https://registry.npmjs.org/",
+              },
+            },
+          })
+      ).toThrow(
+        "Cannot set npmRegistryUrl and yarnRcOptions.npmRegistryServer at the same time."
+      );
+    });
+
+    test("throws an error if npmAccess and npmPublishAccess are both set", () => {
+      const project = new TestProject();
+      expect(
+        () =>
+          new NodePackage(project, {
+            packageManager: NodePackageManager.YARN_BERRY,
+            npmAccess: NpmAccess.PUBLIC,
+            yarnBerryOptions: {
+              yarnRcOptions: {
+                npmPublishAccess: YarnNpmPublishAccess.PUBLIC,
+              },
+            },
+          })
+      ).toThrow(
+        "Cannot set npmAccess and yarnRcOptions.npmPublishAccess at the same time."
+      );
+    });
+  });
+
+  describe("invalid options", () => {
+    describe("using v4", () => {
+      test("throws an error if a v3 setting is used in v4", () => {
+        const project = new TestProject();
+        expect(
+          () =>
+            new NodePackage(project, {
+              packageManager: NodePackageManager.YARN_BERRY,
+              yarnBerryOptions: {
+                version: "4.0.1",
+                yarnRcOptions: {
+                  ignoreCwd: true,
+                  lockfileFilename: "something-else.lock",
+                },
+              },
+            })
+        ).toThrow(
+          "The following options are not available in Yarn >= 4: ignoreCwd, lockfileFilename"
+        );
+      });
+    });
+
+    describe("using v3", () => {
+      test("throws an error if a v4 setting is used in v3", () => {
+        const project = new TestProject();
+        expect(
+          () =>
+            new NodePackage(project, {
+              packageManager: NodePackageManager.YARN_BERRY,
+              yarnBerryOptions: {
+                version: "3.6.4",
+                yarnRcOptions: {
+                  cacheMigrationMode: YarnCacheMigrationMode.ALWAYS,
+                  httpsCaFilePath: "/etc/foo/bar",
+                },
+              },
+            })
+        ).toThrow(
+          "The following options are only available in Yarn v4 and newer: cacheMigrationMode, httpsCaFilePath"
+        );
+      });
+
+      test("throws an error if a v4 checksumBehavior setting is used in v3", () => {
+        const project = new TestProject();
+        expect(
+          () =>
+            new NodePackage(project, {
+              packageManager: NodePackageManager.YARN_BERRY,
+              yarnBerryOptions: {
+                version: "3.6.4",
+                yarnRcOptions: {
+                  checksumBehavior: YarnChecksumBehavior.RESET,
+                },
+              },
+            })
+        ).toThrow(
+          "The YarnChecksumBehavior.RESET is only available in Yarn v4 and newer."
+        );
+      });
+    });
+  });
 });

src/javascript/index.ts
@@ -8,3 +8,4 @@ export * from "./prettier";
 export * from "./projenrc";
 export * from "./typescript-config";
 export * from "./upgrade-dependencies";
+export * from "./yarnrc";

commit 4a2a34e68b4fe70dad759de9d9e9c50da5d1091d

fix(yarn): npmrc and yarnrc cannot be set to the same value (#3091)
Fixes issue introduced with #3048.

We should allow people to set potentially conflicting values to the same value. Previously, I was just checking to see if they were both set at the same time.

---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

src/javascript/node-package.ts
@@ -1519,15 +1519,23 @@ export class NodePackage extends Component {
   }
 
   private checkForConflictingYarnOptions(yarnRcOptions: YarnrcOptions) {
-    if (this.npmAccess && yarnRcOptions.npmPublishAccess) {
+    if (
+      this.npmAccess &&
+      yarnRcOptions.npmPublishAccess &&
+      this.npmAccess.toString() !== yarnRcOptions.npmPublishAccess.toString()
+    ) {
       throw new Error(
-        "Cannot set npmAccess and yarnRcOptions.npmPublishAccess at the same time."
+        `Cannot set npmAccess (${this.npmAccess}) and yarnRcOptions.npmPublishAccess (${yarnRcOptions.npmPublishAccess}) to different values.`
       );
     }
 
-    if (this.npmRegistryUrl && yarnRcOptions.npmRegistryServer) {
+    if (
+      this.npmRegistryUrl &&
+      yarnRcOptions.npmRegistryServer &&
+      this.npmRegistryUrl !== yarnRcOptions.npmRegistryServer
+    ) {
       throw new Error(
-        "Cannot set npmRegistryUrl and yarnRcOptions.npmRegistryServer at the same time."
+        `Cannot set npmRegistryUrl (${this.npmRegistryUrl}) and yarnRcOptions.npmRegistryServer (${yarnRcOptions.npmRegistryServer}) to different values.`
       );
     }
   }

test/javascript/node-package.test.ts
@@ -798,7 +798,7 @@ describe("yarn berry", () => {
   });
 
   describe("conflicting options", () => {
-    test("throws an error if npmRegistryUrl and npmRegistryServer are both set", () => {
+    test("throws an error if npmRegistryUrl and npmRegistryServer are set to different values", () => {
       const project = new TestProject();
       expect(
         () =>
@@ -807,16 +807,16 @@ describe("yarn berry", () => {
             npmRegistryUrl: "https://registry.npmjs.org/",
             yarnBerryOptions: {
               yarnRcOptions: {
-                npmRegistryServer: "https://registry.npmjs.org/",
+                npmRegistryServer: "https://npm.pkg.github.com",
               },
             },
           })
       ).toThrow(
-        "Cannot set npmRegistryUrl and yarnRcOptions.npmRegistryServer at the same time."
+        "Cannot set npmRegistryUrl (https://registry.npmjs.org/) and yarnRcOptions.npmRegistryServer (https://npm.pkg.github.com) to different values."
       );
     });
 
-    test("throws an error if npmAccess and npmPublishAccess are both set", () => {
+    test("throws an error if npmAccess and npmPublishAccess are set to different values", () => {
       const project = new TestProject();
       expect(
         () =>
@@ -825,12 +825,12 @@ describe("yarn berry", () => {
             npmAccess: NpmAccess.PUBLIC,
             yarnBerryOptions: {
               yarnRcOptions: {
-                npmPublishAccess: YarnNpmPublishAccess.PUBLIC,
+                npmPublishAccess: YarnNpmPublishAccess.RESTRICTED,
               },
             },
           })
       ).toThrow(
-        "Cannot set npmAccess and yarnRcOptions.npmPublishAccess at the same time."
+        "Cannot set npmAccess (public) and yarnRcOptions.npmPublishAccess (restricted) to different values."
       );
     });
   });