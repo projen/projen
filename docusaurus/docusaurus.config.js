4e4f7e29d9ab4b47f9fa5b6eb2e898e346adb7ba
Matthew Bonig <matthew.bonig@gmail.com>
2023-12-12 12:24:15 -0700

    adding base url for testing purposes

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://projen.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/projen', // but no...
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
