8351bdcec61a3dc2b3ec69aaf707555a3a83dbb9
Derek Kershner <25798427+dkershner6@users.noreply.github.com>
2024-01-01 12:22:01 -0800

    No _ on findTargetGitHubForWorkflow
    Co-authored-by: Momo Kornher <mail@moritzkornher.de>

--- src/release/release.ts
+++ src/release/release.ts
@@ -715,7 +715,7 @@ export class Release extends Component {
     }
   }
 
-  private _findTargetGitHubForWorkflow(): GitHub | undefined {
+  private findTargetGitHubForWorkflow(): GitHub | undefined {
     if (!this.github) {
       return;
     }
