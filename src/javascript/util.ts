projen/projen

commit 2aa861deb9f6bd7258be31551ede6b951ca23832

fix(node): directory separators are inconsistent between platforms in tasks.json and package.json (#3387)
Related to https://github.com/projen/projen/issues/3284#issuecomment-1952483404 

## Changelog

1. Use POSIX path for tasks with lambda entrypoint.

![Screenshot 2024-02-23 162624](https://github.com/projen/projen/assets/22875166/5bf19a20-b360-4b66-a4cf-7c264deb810d)

2. Normalize `NodeProject.artifactsJavascriptDirectory`, resulting in `dist\\js` being written on Windows instead of `dist/js`.

![Screenshot 2024-02-23 162616](https://github.com/projen/projen/assets/22875166/951670b7-a83e-4631-91c3-58028522f9dc)




---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

src/javascript/util.ts
@@ -1,5 +1,5 @@
 import { existsSync, readFileSync } from "fs";
-import { basename, dirname, extname, join, sep, resolve } from "path";
+import { basename, dirname, extname, join, sep, resolve, posix } from "path";
 import * as semver from "semver";
 import { NodePackage } from "./node-package";
 import { Project } from "../project";
@@ -30,7 +30,7 @@ export function renderBundleName(entrypoint: string) {
   const p = parts.join(sep);
   const dir = dirname(p);
   const base = basename(p, extname(p));
-  return join(dir, base);
+  return posix.join(dir, base);
 }
 
 /**

src/javascript/node-project.ts
@@ -1,4 +1,4 @@
-import { join, relative } from "path";
+import { relative, posix } from "path";
 import { Bundler, BundlerOptions } from "./bundler";
 import { Jest, JestOptions } from "./jest";
 import { LicenseChecker, LicenseCheckerOptions } from "./license-checker";
@@ -49,7 +49,7 @@ import {
 } from "../release";
 import { filteredRunsOnOptions } from "../runner-options";
 import { Task } from "../task";
-import { deepMerge } from "../util";
+import { deepMerge, normalizePersistedPath } from "../util";
 import { ensureRelativePathStartsWithDot } from "../util/path";
 import { Version } from "../version";
 
@@ -486,7 +486,13 @@ export class NodeProject extends GitHubProject {
       options.workflowGitIdentity ?? DEFAULT_GITHUB_ACTIONS_USER;
     this.workflowPackageCache = options.workflowPackageCache ?? false;
     this.artifactsDirectory = options.artifactsDirectory ?? "dist";
-    this.artifactsJavascriptDirectory = join(this.artifactsDirectory, "js");
+    const normalizedArtifactsDirectory = normalizePersistedPath(
+      this.artifactsDirectory
+    );
+    this.artifactsJavascriptDirectory = posix.join(
+      normalizedArtifactsDirectory,
+      "js"
+    );
 
     this.runScriptCommand = (() => {
       switch (this.packageManager) {

commit 49a51ef43ee563d74d6a861cb0bfcff8486ac257

fix(node): some directory separators are still inconsistent between platforms in tasks.json and package.json (#3406)
Related to https://github.com/projen/projen/issues/3284#issuecomment-1964163891

This is the second attempt at fixing the directory separator issue for lambda entrypoints, which was started in https://github.com/projen/projen/pull/3387. 

I tried adding unit tests for `renderBundleName`. Unfortunately, this can't be unit-tested now because NodeJS exports the `path` package for the Linux platform instead of Windows at import time and not at runtime.
https://github.com/nodejs/node/blob/f28ccd3941e70a825786eff7e28f59014ac76ac9/lib/path.js#L1546

I tested it locally in an example project, and it looks like it's fixed now.
![image](https://github.com/projen/projen/assets/22875166/4f98fb92-0a45-4d83-845c-152e48ded763)


---
By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

src/javascript/util.ts
@@ -27,7 +27,7 @@ export function renderBundleName(entrypoint: string) {
     parts.shift(); // just remove 'src' if its the first element for ergonomics
   }
 
-  const p = parts.join(sep);
+  const p = parts.join(posix.sep);
   const dir = dirname(p);
   const base = basename(p, extname(p));
   return posix.join(dir, base);