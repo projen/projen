ca6709d5b1d03949ae97f43a0d1c30e2b7a17018
Derek Kershner <dkershner@dkershner.com>
2024-03-29 09:53:23 -0700

    fix: better test description

--- test/javascript/upgrade-dependencies.test.ts
+++ test/javascript/upgrade-dependencies.test.ts
@@ -96,7 +96,7 @@ test("upgrades command includes all dependencies", () => {
   `);
 });
 
-test("ncu upgrade command does not include dependencies with pinned versions, but package manager upgrade does", () => {
+test("ncu upgrade command does not include dependencies with any version constraint, but package manager upgrade does", () => {
   const project = createProject({
     deps: ["some-dep@10.0.0"],
   });
