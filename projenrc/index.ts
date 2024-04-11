5c6720ef0c9f78c3b3512364c0c5221da550a90f
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2024-04-11 22:40:29 +0200

    feat: add continue-on-error to build

--- projenrc/index.ts
+++ projenrc/index.ts
@@ -346,11 +346,15 @@ export function setupGithubWorkflowWindows(project: NodeProject) {
   );
   // console.log(buildWorkflow);
   buildWorkflow?.patch(
-    JsonPatch.add("/jobs/build/strategy", {
-      matrix: {
-        runner: ["ubuntu-latest", "windows-latest"],
+    JsonPatch.add("/jobs/build", {
+      "runs-on": "${{ matrix.runner }}",
+      "continue-on-error": "${{ matrix.experimental }}",
+      strategy: {
+        matrix: {
+          runner: ["ubuntu-latest", "windows-latest"],
+          experimental: [false, true],
+        },
       },
-    }),
-    JsonPatch.add("/jobs/build/runs-on", "${{ matrix.runner }}")
+    })
   );
 }
