2881bccbd7b866efadd9049b5b917790cc922130
Momo Kornher <mail@moritzkornher.de>
2024-06-07 10:12:51 +0100

    Apply suggestions from code review

--- src/task-runtime.ts
+++ src/task-runtime.ts
@@ -89,7 +89,7 @@ class RunTask {
     private readonly task: TaskSpec,
     parents: string[] = [],
     args: Array<string | number> = [],
-    envParam: { [name: string]: string } = {}
+    env: { [name: string]: string } = {}
   ) {
     this.workdir = task.cwd ?? this.runtime.workdir;
 
@@ -100,7 +100,7 @@ class RunTask {
       return;
     }
 
-    this.env = this.resolveEnvironment(envParam, parents);
+    this.env = this.resolveEnvironment(env, parents);
 
     const envlogs = [];
     for (const [k, v] of Object.entries(this.env)) {
