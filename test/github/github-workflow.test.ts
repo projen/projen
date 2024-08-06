40621b171e581a04b1367ffded3e4cf2ced82c39
epolon <epolon@amazon.com>
2024-08-06 14:40:04 +0300

    mid work

--- test/github/github-workflow.test.ts
+++ test/github/github-workflow.test.ts
@@ -6,31 +6,42 @@ import { synthSnapshot, TestProject } from "../util";
 describe("github-workflow", () => {
   const workflowName = "test-workflow";
 
-  test("Default concurrency allowed", () => {
+  test("conccurency is not set by default", () => {
     const project = new TestProject();
 
     new GithubWorkflow(project.github!, workflowName);
 
     const snapshot = synthSnapshot(project);
 
-    expect(snapshot[`.github/workflows/${workflowName}.yml`]).not.toContain(
-      "concurrency"
-    );
+    expect(snapshot[`.github/workflows/${workflowName}.yml`]).toMatchSnapshot();
   });
 
-  test("concurrency set", () => {
-    const concurrencyName = "my-concurrency";
+  test("concurrency defaults", () => {
     const project = new TestProject();
 
     new GithubWorkflow(project.github!, workflowName, {
-      concurrency: concurrencyName,
+      limitConcurrency: true,
     });
 
     const snapshot = synthSnapshot(project);
 
-    expect(snapshot[`.github/workflows/${workflowName}.yml`]).toContain(
-      `concurrency: ${concurrencyName}`
-    );
+    expect(snapshot[`.github/workflows/${workflowName}.yml`]).toMatchSnapshot();
+  });
+
+  test("can override concurrency defaults", () => {
+    const project = new TestProject();
+
+    new GithubWorkflow(project.github!, workflowName, {
+      limitConcurrency: true,
+      concurrencyOptions: {
+        group: "custom-group",
+        cancelInProgress: true,
+      },
+    });
+
+    const snapshot = synthSnapshot(project);
+
+    expect(snapshot[`.github/workflows/${workflowName}.yml`]).toMatchSnapshot();
   });
 
   test("workflow job calling a reusable workflow", () => {
