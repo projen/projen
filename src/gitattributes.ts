b838efcd167bd663f09bedf1e02fde8f6dc5c316
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2024-08-22 06:41:47 -0400

    feat(gitattributes)!: support endOfLine with Linux default (#3784)
    Fixes https://github.com/projen/projen/issues/3545
    
    Adds endOfLine option with LF default to avoid git mutations. This default can be overridden by later lines set by the user. It can be disabled by setting it to `{ endOfLine: EndOfLine.NONE}`, in case of a breaking change or other reason. Added tests for this new option and for other logic of `GitAttributesFile` to improve coverage. 
    
    
    BREAKING CHANGE: A new gitattribute is added by default to normalize line endings (eol) of text files to LF. This prevents mutations when Windows and Linux/macOS users are working on the same project. To revert back to the previous behavior (line endings behavior is defined by user config), add the option `endOfLine: EndOfLine.NONE` to the project.
    
    ---
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- src/gitattributes.ts
+++ src/gitattributes.ts
@@ -2,6 +2,46 @@ import { IConstruct } from "constructs";
 import { IResolver, FileBase } from "./file";
 
 /**
+ * The end of line characters supported by git.
+ */
+export enum EndOfLine {
+  /**
+   * Maintain existing (mixed values within one file are normalised by looking
+   * at what's used after the first line)
+   */
+  AUTO = "auto",
+
+  /**
+   * Carriage Return + Line Feed characters (\r\n), common on Windows
+   */
+  CRLF = "crlf",
+
+  /**
+   * Line Feed only (\n), common on Linux and macOS as well as inside git repos
+   */
+  LF = "lf",
+
+  /**
+   * Disable and do not configure the end of line character
+   */
+  NONE = "none",
+}
+
+/**
+ * Options for `GitAttributesFile`.
+ */
+export interface GitAttributesFileOptions {
+  /**
+   * The default end of line character for text files.
+   *
+   * endOfLine it's useful to keep the same end of line between Windows and Unix operative systems for git checking/checkout operations. Hence, it can avoid simple repository mutations consisting only of changes in the end of line characters. It will be set in the first line of the .gitattributes file to make it the first match with high priority but it can be overriden in a later line. Can be disabled by setting explicitly: `{ endOfLine: EndOfLine.NONE }`.
+   *
+   * @default EndOfLine.LF
+   */
+  readonly endOfLine?: EndOfLine;
+}
+
+/**
  * Assign attributes to file names in a git repository.
  *
  * @see https://git-scm.com/docs/gitattributes
@@ -9,10 +49,29 @@ import { IResolver, FileBase } from "./file";
 export class GitAttributesFile extends FileBase {
   private readonly attributes = new Map<string, Set<string>>();
 
-  public constructor(scope: IConstruct) {
+  /**
+   * The default end of line character for text files.
+   */
+  public readonly endOfLine: EndOfLine;
+
+  public constructor(scope: IConstruct, options?: GitAttributesFileOptions) {
     super(scope, ".gitattributes", {
       editGitignore: false,
     });
+
+    this.endOfLine = options?.endOfLine ?? EndOfLine.LF;
+
+    if (this.endOfLine != EndOfLine.NONE) {
+      let endOfLineAttributes = [`text=auto`];
+
+      if (this.endOfLine != EndOfLine.AUTO) {
+        endOfLineAttributes.push(`eol=${this.endOfLine}`);
+      }
+
+      // Setting a default end of line for all text files in the repository
+      // This line should be the first one in order to use it as a default for text files and allow for overriding in later lines
+      this.addAttributes("*", ...endOfLineAttributes);
+    }
   }
 
   /**
