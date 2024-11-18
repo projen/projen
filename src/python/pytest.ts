7f322686f9f37b60032d008ebfbc9ff9d3dd779f
Mike <mike@graywind.org>
2024-11-18 08:32:11 -0600

    Update src/python/pytest.ts
    Co-authored-by: Momo Kornher <mail@moritzkornher.de>

--- src/python/pytest.ts
+++ src/python/pytest.ts
@@ -15,7 +15,7 @@ export interface PytestOptions {
    * Typically the same directory where project tests will be located.
    *
    * @default 'tests'
-   * @deprecated Reference `testdir` on the project instead.
+   * @deprecated Reference `testdir` on the project instead. To change the directory where tests are discovered from, use `testMatch`.
    */
   readonly testdir?: string;
 
