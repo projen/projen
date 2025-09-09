ae4a0d3578aae43b91d3982681a0fe33bf9cfab9
Momo Kornher <kornherm@amazon.co.uk>
2025-09-09 14:58:46 +0100

    fix only test supported versions

--- .projenrc.ts
+++ .projenrc.ts
@@ -87,6 +87,8 @@ const project = new JsiiProject({
     "json2jsii",
     // Needed to generate biome config
     "@biomejs/biome@^2",
+    // used to get current node versions in tests
+    "@jsii/check-node",
   ],
 
   peerDeps: ["constructs@^10.0.0"],
