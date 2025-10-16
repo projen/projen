620b1568bb5bb94f6c6c99b8a9b3e942825f0763
Amir Szekely <kichik@gmail.com>
2025-10-16 21:07:01 +0000

    bring back patch due to workflowNodeVersion: "20.9.0"

--- .projenrc.ts
+++ .projenrc.ts
@@ -18,7 +18,7 @@ import {
   JsiiFromJsonSchema,
   JsonConst,
 } from "./projenrc";
-import { ProjectTree, ReleasableCommits } from "./src";
+import { JsonPatch, ProjectTree, ReleasableCommits } from "./src";
 import { JsiiProject } from "./src/cdk";
 import { tryResolveDependencyVersion } from "./src/javascript/util";
 
@@ -172,6 +172,12 @@ const project = new JsiiProject({
     allow: ["MIT", "ISC", "BSD", "BSD-2-Clause", "BSD-3-Clause", "Apache-2.0"],
   },
 });
+
+project.github
+  ?.tryFindWorkflow("release")
+  ?.file?.patch(
+    JsonPatch.replace("/jobs/release_npm/steps/0/with/node-version", "24.x")
+  );
 
 setupCheckLicenses(project);
 
