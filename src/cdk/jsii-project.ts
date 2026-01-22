b8efbcbc15cc9959bcda927aa4e642f875547702
Momo Kornher <kornherm@amazon.co.uk>
2026-01-22 11:59:19 +0000

    Apply suggestion from @mrgrain

--- src/cdk/jsii-project.ts
+++ src/cdk/jsii-project.ts
@@ -22,7 +22,7 @@ import { TypeScriptProject, TypeScriptProjectOptions } from "../typescript";
 import { deepMerge } from "../util";
 
 const EMAIL_REGEX =
-  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  /^[^\s@]+@[^\s@]+$/;
 const URL_REGEX =
   /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
 
