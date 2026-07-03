1c03547c727f7df37435b029f40eae00877de57a
Dave Shepherd <dave.shepherd@endor.me.uk>
2026-07-03 09:16:27 +0100

    chore(mergify): improve documentation for MergifyCommitMessageFormat

--- src/github/mergify.ts
+++ src/github/mergify.ts
@@ -18,7 +18,11 @@ export type MergifyCondition = string | MergifyConditionalOperator;
 export type MergifyCommitTrailer =
   "co-authored-by" | "approved-by" | "merged-by";
 
-export interface MergifyCommitMessageFormat {
+/**
+  * Declarative configuration for Mergify `commit_message_format`.
+  * @see https://docs.mergify.com/workflow/actions/merge/#customizing-the-commit-message
+  */
+ export interface MergifyCommitMessageFormat {
   /**
    * Commit title format.
    *
